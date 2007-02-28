750de41ed31f54647cf851ff297e762dda79ed49
Jason Alexander <jason.alexander@gmail.com>
2007-02-28 04:48:06 +0000

    git-svn-id: https://dotnetopenid.googlecode.com/svn/trunk@24 01efa1a6-402a-0410-b0ae-47b76eba00f0

---
+++ source/OpenIdTests/AssocationTestSuite.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Janrain.OpenId;
+
+namespace OpenIdTests
+{
+    [TestFixture]
+    public class AssocationTestSuite
+    {
+
+        [Test]
+        public void SerializeDeserialize()
+        {
+            TimeSpan expiresIn = new TimeSpan(0, 0, 600);
+            string handle = "handle";
+            byte[] secret = ASCIIEncoding.ASCII.GetBytes("secret");
+            
+            
+            Association assoc = new HMACSHA1Association(handle, secret, expiresIn);
+
+            byte[] s = assoc.Serialize();
+
+            Association assoc2 = assoc.Deserialize(s);
+            
+
+            Assert.IsTrue(assoc.Equals(assoc2));
+            Assert.AreEqual(assoc.GetHashCode(), assoc2.GetHashCode());
+
+        }
+
+    }
+}
---
+++ source/OpenIdTests/MemoryStoreTestSuite.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Web;
+using Org.Mentalis.Security.Cryptography;
+using System.Text;
+using NUnit.Framework;
+using Janrain.OpenId;
+using Janrain.OpenId.Store;
+
+
+
+namespace OpenIdTests
+{
+    [TestFixture]
+    public class MemoryStoreTestSuite
+    {
+
+        // This requires Consumer; finished soon.
+    }
+}
