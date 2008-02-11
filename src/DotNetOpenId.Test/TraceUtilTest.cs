d0e21662250bdf849d191739cd6278d87a71927e
Andrew Arnott <andrewarnott@gmail.com>
2008-02-11 17:35:54 +0000

    Added TraceUtil class test.
    git-svn-id: https://dotnetopenid.googlecode.com/svn/trunk@128 01efa1a6-402a-0410-b0ae-47b76eba00f0

---
+++ src/DotNetOpenId.Test/TraceUtilTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Permissions;
+
+namespace DotNetOpenId.Test {
+    [TestFixture]
+    public class TraceUtilTest {
+
+        string sniffTrace(string tracemessage) {
+            var sb = new StringBuilder();
+            using (TraceListener tl = new TextWriterTraceListener(new StringWriter(sb))) {
+                Trace.Listeners.Add(tl);
+                try {
+                    TraceUtil.ConsumerTrace(tracemessage);
+                } finally {
+                    Trace.Listeners.Remove(tl);
+                }
+            }
+            return sb.ToString();
+        }
+
+        [Test]
+        public void TraceTest() {
+            Assert.IsTrue(sniffTrace("TESTTRACE").Contains("TESTTRACE"));
+        }
+    }
+}
