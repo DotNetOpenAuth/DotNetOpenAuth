a4886a36ab76514ede1d0026023cb9bbd9f0c80d
Andrew Arnott <andrewarnott@gmail.com>
2008-02-12 06:59:25 +0000

    Set up comments so they are xml doc comments.
    git-svn-id: https://dotnetopenid.googlecode.com/svn/trunk@134 01efa1a6-402a-0410-b0ae-47b76eba00f0

--- src/DotNetOpenId/Session/ISessionState.cs
+++ src/DotNetOpenId/Session/ISessionState.cs
@@ -6,56 +6,37 @@ namespace DotNetOpenId.Session
 {
     public interface ISessionState
     {
-        // Summary:
-        //     Gets or sets a session value by numerical index.
-        //
-        // Parameters:
-        //   index:
-        //     The numerical index of the session value.
-        //
-        // Returns:
-        //     The session-state value stored at the specified index.
+        /// <summary>
+        /// Gets or sets a session value by numerical index.
+        /// </summary>
+        /// <param name="index">The numerical index of the session value.</param>
+        /// <returns>The session-state value stored at the specified index.</returns>
         object this[int index] { get; set; }
-        //
-        // Summary:
-        //     Gets or sets a session value by name.
-        //
-        // Parameters:
-        //   name:
-        //     The key name of the session value.
-        //
-        // Returns:
-        //     The session-state value with the specified name.
+        /// <summary>
+        /// Gets or sets a session value by name.
+        /// </summary>
+        /// <param name="name">The key name of the session value.</param>
+        /// <returns>The session-state value with the specified name.</returns>
         object this[string name] { get; set; }
-        //
-        // Summary:
-        //     Adds a new item to the session-state collection.
-        //
-        // Parameters:
-        //   name:
-        //     The name of the item to add to the session-state collection.
-        //
-        //   value:
-        //     The value of the item to add to the session-state collection.
+        /// <summary>
+        /// Adds a new item to the session-state collection.
+        /// </summary>
+        /// <param name="name">The name of the item to add to the session-state collection.</param>
+        /// <param name="value">The value of the item to add to the session-state collection.</param>
         void Add(string name, object value);
-        //
-        // Summary:
-        //     Removes all keys and values from the session-state collection.
+        /// <summary>
+        /// Removes all keys and values from the session-state collection.
+        /// </summary>
         void Clear();
-        //
-        // Summary:
-        //     Deletes an item from the session-state collection.
-        //
-        // Parameters:
-        //   name:
-        //     The name of the item to delete from the session-state collection.
+        /// <summary>
+        /// Deletes an item from the session-state collection.
+        /// </summary>
+        /// <param name="name">The name of the item to delete from the session-state collection.</param>
         void Remove(string name);
-        //
-        // Summary:
-        //     Gets the number of items in the session-state collection.
-        //
-        // Returns:
-        //     The number of items in the collection.
+        /// <summary>
+        /// Gets the number of items in the session-state collection.
+        /// </summary>
+        /// <value>The number of items in the collection.</value>
         int Count { get; }
     }
 }
