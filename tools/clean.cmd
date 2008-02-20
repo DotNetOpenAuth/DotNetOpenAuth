59a972791cc132ba9bd8dc8756621bb04e3e6f51
Andrew Arnott <andrewarnott@gmail.com>
2008-02-20 05:55:24 +0000

    Fixed web project building with the new projects.
    git-svn-id: https://dotnetopenid.googlecode.com/svn/trunk@168 01efa1a6-402a-0410-b0ae-47b76eba00f0

--- tools/clean.cmd
+++ tools/clean.cmd
@@ -3,8 +3,12 @@ REM %~dp0 returns the directory that the script resides in
 pushd %~dp0..
 IF EXIST bin rd /s /q bin
 IF EXIST drop rd /s /q drop
+
 IF EXIST samples\consumerportal\obj rd /s /q samples\consumerportal\obj
+IF EXIST samples\consumerportal\bin rd /s /q samples\consumerportal\bin
 IF EXIST samples\providerportal\obj rd /s /q samples\providerportal\obj
-IF EXIST src\dotnetopenid\obj rd /s /q src\dotnetopenid\obj 
-IF EXIST src\dotnetopenid.test\obj rd /s /q src\dotnetopenid.test\obj
+IF EXIST samples\providerportal\bin rd /s /q samples\providerportal\bin
+IF EXIST src\dotnetopenid\obj       rd /s /q src\dotnetopenid\obj
+IF EXIST src\dotnetopenid.test\obj  rd /s /q src\dotnetopenid.test\obj
+
 popd
