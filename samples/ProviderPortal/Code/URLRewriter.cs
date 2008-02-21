dbc308753e8a22a0a0a709013d84f557a1024e2e
Andrew Arnott <andrewarnott@gmail.com>
2008-02-21 02:05:34 +0000

    Moved urlrewrite section of providerportal sample outside the system.web section.  Fixes issue 38.
    git-svn-id: https://dotnetopenid.googlecode.com/svn/trunk@172 01efa1a6-402a-0410-b0ae-47b76eba00f0

--- samples/ProviderPortal/Code/URLRewriter.cs
+++ samples/ProviderPortal/Code/URLRewriter.cs
@@ -13,46 +13,57 @@ using DotNetOpenId.Provider;
 using System.Diagnostics;
 
 // nicked from http://www.codeproject.com/aspnet/URLRewriter.asp
-
+namespace ProviderPortal {
 	public class URLRewriter : IConfigurationSectionHandler {
-		protected XmlNode _oRules=null;
+		protected XmlNode _oRules = null;
 
-        protected URLRewriter() { }
+		protected URLRewriter() { }
 
 		public string GetSubstitution(string zPath) {
 			Regex oReg;
 
-			foreach(XmlNode oNode in _oRules.SelectNodes("rule")) {
-				oReg=new Regex(oNode.SelectSingleNode("url/text()").Value,RegexOptions.IgnoreCase);
-				Match oMatch=oReg.Match(zPath);
+			foreach (XmlNode oNode in _oRules.SelectNodes("rule")) {
+				// get the url and rewrite nodes
+				XmlNode oUrlNode = oNode.SelectSingleNode("url");
+				XmlNode oRewriteNode = oNode.SelectSingleNode("rewrite");
 
-				if(oMatch.Success) {
-					return oReg.Replace(zPath,oNode.SelectSingleNode("rewrite/text()").Value);
+				// check validity of the values
+				if (oUrlNode == null || string.IsNullOrEmpty(oUrlNode.InnerText)
+					|| oRewriteNode == null || string.IsNullOrEmpty(oRewriteNode.InnerText)) {
+					Trace.TraceWarning("Invalid urlrewrites rule discovered in web.config file.");
+					continue;
+				}
+
+				oReg = new Regex(oUrlNode.InnerText, RegexOptions.IgnoreCase);
+
+				// if match, return the substitution
+				Match oMatch = oReg.Match(zPath);
+				if (oMatch.Success) {
+					return oReg.Replace(zPath, oRewriteNode.InnerText);
 				}
 			}
 
-			return zPath;
+			return null; // no rewrite
 		}
 
 		public static void Process() {
-            URLRewriter oRewriter = (URLRewriter)System.Configuration.ConfigurationManager.GetSection("system.web/urlrewrites");
+			URLRewriter oRewriter = (URLRewriter)System.Configuration.ConfigurationManager.GetSection("urlrewrites");
 
-			string zSubst=oRewriter.GetSubstitution(HttpContext.Current.Request.Path);
+			string zSubst = oRewriter.GetSubstitution(HttpContext.Current.Request.Path);
 
 			Trace.TraceInformation("Rewriting url '{0}' to '{1}' ", HttpContext.Current.Request.Url, zSubst);
 
-			if(zSubst.Length>0) {
+			if (!string.IsNullOrEmpty(zSubst)) {
 				HttpContext.Current.RewritePath(zSubst);
 			}
 		}
 
 		#region Implementation of IConfigurationSectionHandler
-		public object Create(object parent, object configContext, XmlNode section) {			
-			_oRules=section;
-
-			// TODO: Compile all Regular Expressions
+		public object Create(object parent, object configContext, XmlNode section) {
+			_oRules = section;
 
 			return this;
 		}
 		#endregion
 	}
+}
