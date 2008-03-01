5564c633ff8d813b037f8b9f9531f6500bd28c22
Andrew Arnott <andrewarnott@gmail.com>
2008-03-01 20:27:02 +0000

    Altered some permission demands in 0.1 and trunk branches.
    git-svn-id: https://dotnetopenid.googlecode.com/svn/branches/0.1@258 01efa1a6-402a-0410-b0ae-47b76eba00f0

--- source/Janrain.OpenId/Properties/AssemblyInfo.cs
+++ source/Janrain.OpenId/Properties/AssemblyInfo.cs
@@ -44,15 +44,18 @@ using System.Net;
 
 // We need to be allowed to execute code.  Besides, it gives a good baseline RequestMinimum permission.
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
-// Allows the consumer to call out to the web server
-[assembly: WebPermission(SecurityAction.RequestMinimum, ConnectPattern=".*")]
+// Allows the consumer to call out to the web server.  This is unnecessary in provider-only scenarios.
+// Note: we don't use a single demand for https?://.* because the regex pattern must exactly
+// match the one used by hosting providers.  Listing them individually seems to be more common.
+[assembly: WebPermission(SecurityAction.RequestMinimum, ConnectPattern = @"http://.*")]
+[assembly: WebPermission(SecurityAction.RequestMinimum, ConnectPattern = @"https://.*")]
 // Allows hosting this assembly in an ASP.NET setting.  Not all applications
 // will host this using ASP.NET, so this is optional.  Besides, we need at least
 // one optional permission to activate CAS permission shrinking.
 [assembly: AspNetHostingPermission(SecurityAction.RequestOptional, Level = AspNetHostingPermissionLevel.Medium)]
 
 // The following are only required for diagnostic logging (Trace.Write, Debug.Assert, etc.).
-#if DEBUG
+#if TRACE
 [assembly: KeyContainerPermission(SecurityAction.RequestMinimum, Unrestricted = true)]
 [assembly: ReflectionPermission(SecurityAction.RequestMinimum, MemberAccess = true)]
 [assembly: RegistryPermission(SecurityAction.RequestMinimum, Unrestricted = true)]
