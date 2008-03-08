49bb1462e87825e0dbb42b5cc945d2af5107652d
Andrew Arnott <andrewarnott@gmail.com>
2008-03-08 18:42:04 +0000

    Added comments.
    git-svn-id: https://dotnetopenid.googlecode.com/svn/trunk@288 01efa1a6-402a-0410-b0ae-47b76eba00f0

--- src/DotNetOpenId/Consumer/AuthenticationRequest.cs
+++ src/DotNetOpenId/Consumer/AuthenticationRequest.cs
@@ -7,8 +7,21 @@ using System.Globalization;
 using System.Web;
 
 namespace DotNetOpenId.Consumer {
+	/// <summary>
+	/// Indicates the mode the Provider should use while authenticating the end user.
+	/// </summary>
 	public enum AuthenticationRequestMode {
+		/// <summary>
+		/// The Provider should use whatever credentials are immediately available
+		/// to determine whether the end user owns the Identifier.  If sufficient
+		/// credentials (i.e. cookies) are not immediately available, the Provider
+		/// should fail rather than prompt the user.
+		/// </summary>
 		Immediate,
+		/// <summary>
+		/// The Provider should determine whether the end user owns the Identifier,
+		/// displaying a web page to the user to login etc., if necessary.
+		/// </summary>
 		Setup
 	}
 
--- src/DotNetOpenId/Consumer/IAuthenticationRequest.cs
+++ src/DotNetOpenId/Consumer/IAuthenticationRequest.cs
@@ -45,13 +45,24 @@ namespace DotNetOpenId.Consumer {
 		/// This method requires an ASP.NET HttpContext.
 		/// </remarks>
 		void RedirectToProvider(bool endResponse);
+		/// <summary>
+		/// Gets/sets the mode the Provider should use during authentication.
+		/// </summary>
 		AuthenticationRequestMode Mode { get; set; }
 		/// <summary>
 		/// Gets the URL the user agent should be redirected to to begin the 
 		/// OpenID authentication process.
 		/// </summary>
 		Uri RedirectToProviderUrl { get; }
+		/// <summary>
+		/// Gets the URL that the user agent will return to after authentication
+		/// completes or fails at the Provider.
+		/// </summary>
 		Uri ReturnToUrl { get; }
+		/// <summary>
+		/// Gets the URL that identifies this consumer web application that
+		/// the Provider will display to the end user.
+		/// </summary>
 		TrustRoot TrustRoot { get; }
 	}
 }
