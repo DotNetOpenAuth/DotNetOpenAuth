baed0c5c4c822a08af2848f86ea7ddaca83736e5
Andrew Arnott <andrewarnott@gmail.com>
2008-03-18 16:42:48 -0700

    Hid irrelevant properties from ASPX users of provider controls.

--- src/DotNetOpenId/Provider/IdentityEndpoint.cs
+++ src/DotNetOpenId/Provider/IdentityEndpoint.cs
@@ -11,6 +11,14 @@ namespace DotNetOpenId.Provider {
 	[ToolboxData("<{0}:IdentityEndpoint runat=server></{0}:IdentityEndpoint>")]
 	public class IdentityEndpoint : WebControl {
 
+		#region Properties
+		[Flags]
+		public enum XrdsUrlLocations {
+			HttpHeader = 0x1,
+			HtmlMeta = 0x2,
+			Both = 0x3
+		}
+
 		const string serverUrlViewStateKey = "ServerUrl";
 		[Bindable(true)]
 		[Category("Behavior")]
@@ -32,6 +40,146 @@ namespace DotNetOpenId.Provider {
 			set {
 				Consumer.OpenIdTextBox.ValidateResolvableUrl(Page, DesignMode, value);
 				ViewState[delegateUrlViewStateKey] = value;
+			}
+		}
+
+		const string xrdsUrlViewStateKey = "XrdsUrl";
+		[Bindable(true)]
+		[Category("Behavior")]
+		public string XrdsUrl {
+			get { return (string)ViewState[xrdsUrlViewStateKey]; }
+			set {
+				Consumer.OpenIdTextBox.ValidateResolvableUrl(Page, DesignMode, value);
+				ViewState[xrdsUrlViewStateKey] = value;
+			}
+		}
+
+		const XrdsUrlLocations xrdsAdvertisementDefault = XrdsUrlLocations.HttpHeader;
+		const string xrdsAdvertisementViewStateKey = "XrdsAdvertisement";
+		[Bindable(true)]
+		[Category("Behavior")]
+		[DefaultValue(xrdsAdvertisementDefault)]
+		[Description("Where the XRDS document URL is advertised in the web response.")]
+		public XrdsUrlLocations XrdsAdvertisement {
+			get {
+				return ViewState[xrdsAdvertisementViewStateKey] == null ?
+					xrdsAdvertisementDefault : (XrdsUrlLocations)ViewState[xrdsAdvertisementViewStateKey];
+			}
+			set { ViewState[xrdsAdvertisementViewStateKey] = value; }
+		}
+
+		const bool xrdsAutoRedirectDefault = true;
+		const string xrdsAutoRedirectViewStateKey = "XrdsAutoRedirect";
+		[Bindable(true)]
+		[Category("Behavior")]
+		[DefaultValue(xrdsAutoRedirectDefault)]
+		[Description("Whether XRDS requests should be immediately redirected to the XRDS document.")]
+		public bool XrdsAutoRedirect {
+			get {
+				return ViewState[xrdsAutoRedirectViewStateKey] == null ?
+					xrdsAutoRedirectDefault : (bool)ViewState[xrdsAutoRedirectViewStateKey];
+			}
+			set { ViewState[xrdsAutoRedirectViewStateKey] = value; }
+		}
+		#endregion
+
+		#region Properties to hide
+		[Browsable(false), Bindable(false)]
+		public override string AccessKey {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override string CssClass {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override bool EnableViewState {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override bool Enabled {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override System.Drawing.Color ForeColor {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override System.Drawing.Color BackColor {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override System.Drawing.Color BorderColor {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override Unit BorderWidth {
+			get { return Unit.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override BorderStyle BorderStyle {
+			get { return BorderStyle.None; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override FontInfo Font {
+			get { return null; }
+		}
+		[Browsable(false), Bindable(false)]
+		public override Unit Height {
+			get { return Unit.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override Unit Width {
+			get { return Unit.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override short TabIndex {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override string ToolTip {
+			get { return string.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override string SkinID {
+			get { return string.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override bool EnableTheming {
+			get { return false; }
+			set { throw new NotSupportedException(); }
+		}
+		#endregion
+
+		protected override void OnLoad(EventArgs e) {
+			base.OnLoad(e);
+
+			if (!Page.IsPostBack) {
+				if (!string.IsNullOrEmpty(XrdsUrl) && XrdsAutoRedirect) {
+					// Check for the presence of an accept types header that is looking
+					// for the XRDS document specifically.
+					if (Page.Request.AcceptTypes != null &&
+						Array.IndexOf(Page.Request.AcceptTypes, Janrain.Yadis.Yadis.CONTENT_TYPE) >= 0) {
+						// Redirect the caller immediately and avoid sending the whole
+						// web page's contents to the client since it isn't interested
+						// anyway.
+						Page.Response.Redirect(new Uri(Page.Request.Url, Page.Response.ApplyAppPathModifier(XrdsUrl)).AbsoluteUri, true);
+					}
+				}
 			}
 		}
 
--- src/DotNetOpenId/Provider/ProviderEndpoint.cs
+++ src/DotNetOpenId/Provider/ProviderEndpoint.cs
@@ -17,25 +17,109 @@ namespace DotNetOpenId.Provider {
 			set { HttpContext.Current.Session[pendingAuthenticationRequestKey] = value; }
 		}
 
+		#region Properties to hide
+		[Browsable(false), Bindable(false)]
+		public override bool Visible {
+			get { return false; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override string AccessKey {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override string CssClass {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override bool EnableViewState {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override short TabIndex {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override System.Drawing.Color ForeColor {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override System.Drawing.Color BackColor {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override System.Drawing.Color BorderColor {
+			get { throw new NotSupportedException(); }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override Unit BorderWidth {
+			get { return Unit.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override BorderStyle BorderStyle {
+			get { return BorderStyle.None; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override FontInfo Font {
+			get { return null; }
+		}
+		[Browsable(false), Bindable(false)]
+		public override Unit Height {
+			get { return Unit.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override Unit Width {
+			get { return Unit.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override string ToolTip {
+			get { return string.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override string SkinID {
+			get { return string.Empty; }
+			set { throw new NotSupportedException(); }
+		}
+		[Browsable(false), Bindable(false)]
+		public override bool EnableTheming {
+			get { return false; }
+			set { throw new NotSupportedException(); }
+		}
+		#endregion
+
 		protected override void OnLoad(EventArgs e) {
 			base.OnLoad(e);
 
-			OpenIdProvider openIDServer = new OpenIdProvider();
+			if (Enabled) {
+				OpenIdProvider openIDServer = new OpenIdProvider();
 
-			// determine what incoming message was received
-			if (openIDServer.Request != null) {
-				// process the incoming message appropriately and send the response
-				if (!openIDServer.Request.IsResponseReady) {
-					var idrequest = (CheckIdRequest)openIDServer.Request;
-					PendingAuthenticationRequest = idrequest;
-					OnAuthenticationChallenge(idrequest);
-				} else {
-					PendingAuthenticationRequest = null;
-				}
-				if (openIDServer.Request.IsResponseReady) {
-					openIDServer.Request.Response.Send();
-					Page.Response.End();
-					PendingAuthenticationRequest = null;
+				// determine what incoming message was received
+				if (openIDServer.Request != null) {
+					// process the incoming message appropriately and send the response
+					if (!openIDServer.Request.IsResponseReady) {
+						var idrequest = (CheckIdRequest)openIDServer.Request;
+						PendingAuthenticationRequest = idrequest;
+						OnAuthenticationChallenge(idrequest);
+					} else {
+						PendingAuthenticationRequest = null;
+					}
+					if (openIDServer.Request.IsResponseReady) {
+						openIDServer.Request.Response.Send();
+						Page.Response.End();
+						PendingAuthenticationRequest = null;
+					}
 				}
 			}
 		}
