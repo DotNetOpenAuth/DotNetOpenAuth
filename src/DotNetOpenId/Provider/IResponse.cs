bd62766fecafed76d94751295eb1e7d4fdec21b2
Andrew Arnott <andrewarnott@gmail.com>
2008-03-19 17:22:17 -0700

    Added documentation around Provider interfaces.

--- src/DotNetOpenId/Provider/IResponse.cs
+++ src/DotNetOpenId/Provider/IResponse.cs
@@ -5,10 +5,28 @@ using System.Net;
 using System.Collections.Specialized;
 
 namespace DotNetOpenId.Provider {
+	/// <summary>
+	/// Represents a Provider's response to an OpenId authentication request.
+	/// </summary>
 	public interface IResponse {
+		/// <summary>
+		/// The HTTP status code that should accompany the response.
+		/// </summary>
 		HttpStatusCode Code { get; }
+		/// <summary>
+		/// The HTTP headers that should be added to the response.
+		/// </summary>
 		NameValueCollection Headers { get; }
+		/// <summary>
+		/// The body that should be sent as the response content.
+		/// </summary>
 		byte[] Body { get; }
+		/// <summary>
+		/// Sends the response to the browser.
+		/// </summary>
+		/// <remarks>
+		/// This requires an ASP.NET hosted web site.
+		/// </remarks>
 		void Send();
 	}
 }
