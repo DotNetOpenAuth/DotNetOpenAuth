7d04e4da88a020af558af7a415cd61db0da94be6
Andrew Arnott <andrewarnott@gmail.com>
2008-03-20 06:57:02 -0700

    Updated version number to 2.0.0.*

--- src/DotNetOpenId/Properties/AssemblyInfo.cs
+++ src/DotNetOpenId/Properties/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Web.UI;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.*")]
-[assembly: AssemblyFileVersion("0.2.*")]
+[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.*")]
 
 [assembly: InternalsVisibleTo("DotNetOpenId.Test")]
 
