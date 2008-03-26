4e2ca103fea7686208827964d61be04ba0f29686
Andrew Arnott <andrewarnott@gmail.com>
2008-03-26 07:50:20 -0700

    Cleaned up a bit more of the RP sample.

--- samples/RelyingPartyPortal/login.aspx.cs
+++ samples/RelyingPartyPortal/login.aspx.cs
@@ -1,25 +1,9 @@
 using System;
-using System.Collections.Specialized;
-using System.Data;
-using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 using DotNetOpenId.RelyingParty;
-using DotNetOpenId.Extensions;
 
 public partial class login : System.Web.UI.Page
 {
-    /// <summary>
-    /// Handles the Load event of the Page control.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
     protected void Page_Load(object sender, EventArgs e)
     {
         OpenIdLogin1.Focus();
@@ -29,8 +13,6 @@ public partial class login : System.Web.UI.Page
     /// Fired upon login.
     /// Note, that straight after login, forms auth will redirect the user to their original page. So this page may never be rendererd.
     /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
     protected void OpenIdLogin1_LoggedIn(object sender, OpenIdEventArgs e)
     {
         State.ProfileFields = e.ProfileFields;
