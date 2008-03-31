6a5e1ba4d6c1504fed1f44ca80112edf733f25ad
Andrew Arnott <andrewarnott@gmail.com>
2008-03-30 22:05:31 -0700

    Added some simple OP tests.

--- src/DotNetOpenId.Test/Provider/OpenIdProviderTest.cs
+++ src/DotNetOpenId.Test/Provider/OpenIdProviderTest.cs
@@ -6,10 +6,52 @@ using System.IO;
 using System.Diagnostics;
 using DotNetOpenId.Test.Hosting;
 using System.Text.RegularExpressions;
+using DotNetOpenId.Provider;
+using System.Collections.Specialized;
+using ProviderMemoryStore = DotNetOpenId.AssociationMemoryStore<DotNetOpenId.AssociationRelyingPartyType>;
 
 namespace DotNetOpenId.Test.Provider {
 	[TestFixture]
 	public class OpenIdProviderTest {
+		readonly Uri providerEndpoint = new Uri("http://someendpoint");
+		readonly Uri emptyRequestUrl = new Uri("http://someendpoint/request");
 
+		/// <summary>
+		/// Verifies that without an ASP.NET context, the default constructor fails.
+		/// </summary>
+		[Test, ExpectedException(typeof(InvalidOperationException))]
+		public void CtorDefault() {
+			OpenIdProvider op = new OpenIdProvider();
+		}
+
+		[Test]
+		public void CtorNonDefault() {
+			OpenIdProvider op = new OpenIdProvider(new ProviderMemoryStore(), 
+				providerEndpoint, emptyRequestUrl, new NameValueCollection());
+		}
+
+		[Test, ExpectedException(typeof(ArgumentNullException))]
+		public void CtorNullStore() {
+			OpenIdProvider op = new OpenIdProvider(null, providerEndpoint, 
+				emptyRequestUrl, new NameValueCollection());
+		}
+
+		[Test, ExpectedException(typeof(ArgumentNullException))]
+		public void CtorNullEndpoint() {
+			OpenIdProvider op = new OpenIdProvider(new ProviderMemoryStore(), 
+				null, emptyRequestUrl, new NameValueCollection());
+		}
+
+		[Test, ExpectedException(typeof(ArgumentNullException))]
+		public void CtorNullRequestUrl() {
+			OpenIdProvider op = new OpenIdProvider(new ProviderMemoryStore(), 
+				providerEndpoint, null, new NameValueCollection());
+		}
+
+		[Test, ExpectedException(typeof(ArgumentNullException))]
+		public void CtorNullQuery() {
+			OpenIdProvider op = new OpenIdProvider(new ProviderMemoryStore(), 
+				providerEndpoint, emptyRequestUrl, null);
+		}
 	}
 }
--- src/DotNetOpenId.Test/Provider/ProviderEndpointTest.cs
+++ src/DotNetOpenId.Test/Provider/ProviderEndpointTest.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
 using DotNetOpenId.Test.Hosting;
+using DotNetOpenId.Provider;
 
 namespace DotNetOpenId.Test.Provider {
 	[TestFixture]
 	public class ProviderEndpointTest {
+		[Test]
+		public void Ctor() {
+			ProviderEndpoint pe = new ProviderEndpoint();
+		}
+
+		[Test]
+		public void SimpleEnabledTest() {
+			ProviderEndpoint pe = new ProviderEndpoint();
+			Assert.IsTrue(pe.Enabled);
+			pe.Enabled = false;
+			Assert.IsFalse(pe.Enabled);
+		}
 	}
 }
