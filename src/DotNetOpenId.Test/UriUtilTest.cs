845d21f72dfbc91fe7644bf521087eced50e12b8
Andrew Arnott <andrewarnott@gmail.com>
2008-04-02 21:07:16 -0700

    Whitespace changes.

--- src/DotNetOpenId.Test/UriUtilTest.cs
+++ src/DotNetOpenId.Test/UriUtilTest.cs
@@ -5,28 +5,28 @@ using NUnit.Framework;
 using System.Collections.Specialized;
 
 namespace DotNetOpenId.Test {
-    [TestFixture]
-    public class UriUtilTest {
-        [Test]
-        public void CreateQueryString() {
-            var args = new Dictionary<string, string>();
-            args.Add("a", "b");
-            args.Add("c/d", "e/f");
-            Assert.AreEqual("a=b&c%2fd=e%2ff", UriUtil.CreateQueryString(args));
-        }
+	[TestFixture]
+	public class UriUtilTest {
+		[Test]
+		public void CreateQueryString() {
+			var args = new Dictionary<string, string>();
+			args.Add("a", "b");
+			args.Add("c/d", "e/f");
+			Assert.AreEqual("a=b&c%2fd=e%2ff", UriUtil.CreateQueryString(args));
+		}
 
-        [Test]
-        public void AppendQueryArgs() {
-            UriBuilder uri = new UriBuilder("http://baseline.org/page");
-            var args = new Dictionary<string, string>();
-            args.Add("a", "b");
-            args.Add("c/d", "e/f");
-            UriUtil.AppendQueryArgs(uri, args);
-            Assert.AreEqual("http://baseline.org/page?a=b&c%2fd=e%2ff", uri.Uri.AbsoluteUri);
-            args.Clear();
-            args.Add("g", "h");
-            UriUtil.AppendQueryArgs(uri, args);
-            Assert.AreEqual("http://baseline.org/page?a=b&c%2fd=e%2ff&g=h", uri.Uri.AbsoluteUri);
-        }
-    }
+		[Test]
+		public void AppendQueryArgs() {
+			UriBuilder uri = new UriBuilder("http://baseline.org/page");
+			var args = new Dictionary<string, string>();
+			args.Add("a", "b");
+			args.Add("c/d", "e/f");
+			UriUtil.AppendQueryArgs(uri, args);
+			Assert.AreEqual("http://baseline.org/page?a=b&c%2fd=e%2ff", uri.Uri.AbsoluteUri);
+			args.Clear();
+			args.Add("g", "h");
+			UriUtil.AppendQueryArgs(uri, args);
+			Assert.AreEqual("http://baseline.org/page?a=b&c%2fd=e%2ff&g=h", uri.Uri.AbsoluteUri);
+		}
+	}
 }
