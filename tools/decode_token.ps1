22b08077660efbfa2f38d6248cdf589eed1a0c48
Andrew Arnott <andrewarnott@gmail.com>
2008-04-29 08:20:17 -0700

    Added decode_token.ps1 tool for diagnostics.

---
+++ tools/decode_token.ps1
@@ -0,0 +1,20 @@
+﻿# This is a useful diagnostic tool for when users send in failed positive assertions
+# and you want to read what's in the token parameter.
+
+param (
+	$token = { throw "token or urlToken parameter required" }, 
+	$urlToken
+	)
+
+[void] [Reflection.Assembly]::LoadWithPartialName('System.Web')
+
+if ($urlToken) {
+	$token = [Web.HttpUtility]::UrlDecode($urlToken)
+}
+
+$tokenBytes = [Convert]::FromBase64String($token)
+$siglength = 32
+$decodedToken = [Text.Encoding]::UTF8.GetString($tokenBytes, $siglength, $tokenBytes.length - $siglength)
+
+Write-Host "Decoded token:"
+Write-Host $decodedToken
