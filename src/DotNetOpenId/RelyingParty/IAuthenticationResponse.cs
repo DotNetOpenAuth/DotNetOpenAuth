10e88394ce0afc8b61016ad371067eb7cf77a414
Andrew Arnott <andrewarnott@gmail.com>
2008-07-15 22:52:11 -0700

    Added IAuthenticationResponse.FriendlyIdentifierForDisplay.
    Build break at this point since I only add the member to the interface and not the implementing types.

--- src/DotNetOpenId/RelyingParty/IAuthenticationResponse.cs
+++ src/DotNetOpenId/RelyingParty/IAuthenticationResponse.cs
@@ -28,9 +28,45 @@ namespace DotNetOpenId.RelyingParty {
 		/// <returns>The extension, if it is found.  Null otherwise.</returns>
 		IExtensionResponse GetExtension(Type extensionType);
 		/// <summary>
-		/// An Identifier that the end user claims to own.
+		/// An Identifier that the end user claims to own.  For use with user database storage and lookup.
 		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// This is the secure identifier that should be used for database storage and lookup.
+		/// It is not always friendly (i.e. =Arnott becomes =!9B72.7DD1.50A9.5CCD), but it protects
+		/// user identities against spoofing and other attacks.  
+		/// </para>
+		/// <para>
+		/// For user-friendly identifiers to display, use the 
+		/// <see cref="FriendlyIdentifierForDisplay"/> property.
+		/// </remarks>
 		Identifier ClaimedIdentifier { get; }
+		/// <summary>
+		/// Gets a user-friendly OpenID Identifier for display purposes ONLY.
+		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// This property retains some aspects of the user-supplied identifier that get lost
+		/// in the <see cref="ClaimedIdentifier"/>.  For example, XRIs used as user-supplied
+		/// identifiers (i.e. =Arnott) become unfriendly unique strings (i.e. =!9B72.7DD1.50A9.5CCD).
+		/// For display purposes, such as text on a web page that says "You're logged in as ...",
+		/// this property serves to provide the =Arnott string, or whatever else is the most friendly
+		/// string close to what the user originally typed in.
+		/// </para>
+		/// <para>
+		/// If the user-supplied identifier is a URI, this property will be the URI after all 
+		/// redirects, and with the protocol trimmed off.
+		/// If the user-supplied identifier is an XRI, this property will be the original XRI.
+		/// If the user-supplied identifier is an OpenID Provider identifier (i.e. yahoo.com), 
+		/// this property will be the Claimed Identifier, with the protocol stripped if it is a URI.
+		/// </para>
+		/// <para>
+		/// It is <b>very</b> important that this property <i>never</i> be used for database storage
+		/// or lookup to avoid identity spoofing and other security risks.  For database storage
+		/// and lookup please use the <see cref="ClaimedIdentifier"/> property.
+		/// </para>
+		/// </remarks>
+		//Identifier FriendlyIdentifierForDisplay { get; }
 		/// <summary>
 		/// The detailed success or failure status of the authentication attempt.
 		/// </summary>
