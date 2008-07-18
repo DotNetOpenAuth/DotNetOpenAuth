DotNetOpenAuth/DotNetOpenAuth

commit 42fcca8c60a4aea3061ebcde018c912a6aadc398

Fixed AX store requests so RPs can actually create them.
The necessary constructor to call was internal.  Goo!

src/DotNetOpenId/Extensions/AttributeExchange/AttributeValues.cs
@@ -19,14 +19,12 @@ namespace DotNetOpenId.Extensions.AttributeExchange {
 			Values = new List<string>(1);
 		}
 		internal AttributeValues(string typeUri)
-			: this() {
-			TypeUri = typeUri;
+			: this(typeUri, null) {
 		}
-		internal AttributeValues(string typeUri, params string[] values) {
+		public AttributeValues(string typeUri, params string[] values) {
 			if (string.IsNullOrEmpty(typeUri)) throw new ArgumentNullException("typeUri");
-			if (values == null) throw new ArgumentNullException("values");
 			TypeUri = typeUri;
-			Values = values;
+			Values = values ?? new string[0];
 		}
 
 		/// <summary>

src/DotNetOpenId/Extensions/AttributeExchange/StoreRequest.cs
@@ -30,6 +30,13 @@ namespace DotNetOpenId.Extensions.AttributeExchange {
 			attributesProvided.Add(attribute);
 		}
 		/// <summary>
+		/// Used by the Relying Party to add a given attribute with one or more values 
+		/// to the request for storage.
+		/// </summary>
+		public void AddAttribute(string typeUri, params string[] values) {
+			AddAttribute(new AttributeValues(typeUri, values));
+		}
+		/// <summary>
 		/// Used by the Provider to gets the value(s) associated with a given attribute
 		/// that should be stored.
 		/// </summary>

commit 0397f2560a61f5a97d59abaa166dbe54f0165b98

Fixed bug in AX introduced in 42fcca8c.

src/DotNetOpenId/Extensions/AttributeExchange/AttributeValues.cs
@@ -18,8 +18,10 @@ namespace DotNetOpenId.Extensions.AttributeExchange {
 		internal AttributeValues() {
 			Values = new List<string>(1);
 		}
-		internal AttributeValues(string typeUri)
-			: this(typeUri, null) {
+		internal AttributeValues(string typeUri) {
+			if (string.IsNullOrEmpty(typeUri)) throw new ArgumentNullException("typeUri");
+			TypeUri = typeUri;
+			Values = new List<string>(1);
 		}
 		public AttributeValues(string typeUri, params string[] values) {
 			if (string.IsNullOrEmpty(typeUri)) throw new ArgumentNullException("typeUri");