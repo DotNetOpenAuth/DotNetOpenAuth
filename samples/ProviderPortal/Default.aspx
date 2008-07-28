DotNetOpenAuth/DotNetOpenAuth

commit 30a21d9586af74062af9b77728e1595d8e3735cb

Upgraded 2 primary sample sites to use logo and master pages.

samples/ProviderPortal/Default.aspx
@@ -1,8 +1,7 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" %>
 
 <%@ Import Namespace="DotNetOpenId.Provider" %>
 <%@ Register Assembly="DotNetOpenId" Namespace="DotNetOpenId" TagPrefix="openid" %>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <script runat="server">
 	protected void sendAssertionButton_Click(object sender, EventArgs e) {
@@ -20,18 +19,9 @@
 	}
 </script>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-	<openid:XrdsPublisher runat="server" XrdsUrl="~/op_xrds.aspx" />
-	<title>OpenID Provider, by DotNetOpenId</title>
-</head>
-<body>
-	<form id="form1" runat="server">
-	<h1>
-		OpenID Provider
-	</h1>
+<asp:Content runat=server ContentPlaceHolderID=Main>
 	<h2>
-		Provided by <a href="http://dotnetopenid.googlecode.com">DotNetOpenId</a>
+		Provider
 	</h2>
 	<p>
 		Welcome. This site doesn't do anything more than simple authentication of users.
@@ -59,6 +49,4 @@
 		</LoggedInTemplate>
 	</asp:LoginView>
 	<asp:LoginStatus runat="server" />
-	</form>
-</body>
-</html>
+</asp:Content>

samples/RelyingPartyPortal/Default.aspx
@@ -1,26 +1,14 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" %>
 
 <%@ Register Assembly="DotNetOpenId" Namespace="DotNetOpenId" TagPrefix="openid" %>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-	<title>OpenID Relying Party, by DotNetOpenId</title>
-	<openid:XrdsPublisher runat="server" XrdsUrl="~/xrds.aspx" />
-</head>
-<body>
-	<form id="form1" runat="server">
-	<h1>
-		OpenID Relying Party
-	</h1>
-	<h2>
-		Provided by <a href="http://dotnetopenid.googlecode.com">DotNetOpenId</a>
-	</h2>
-	<p>
-		Visit the
-		<asp:HyperLink runat="server" NavigateUrl="~/MembersOnly/Default.aspx" Text="Members Only" />
-		area. (This will trigger a login demo).
-	</p>
-	<asp:LoginStatus runat="server" />
-	</form>
-</body>
-</html>
+<asp:Content runat="server" ContentPlaceHolderID="head">
+	<openid:XrdsPublisher ID="XrdsPublisher1" runat="server" XrdsUrl="~/xrds.aspx" />
+</asp:Content>
+<asp:Content runat="server" ContentPlaceHolderID="main">
+	<h2>Relying Party </h2>
+	<p>Visit the
+		<asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/MembersOnly/Default.aspx"
+			Text="Members Only" />
+		area. (This will trigger a login demo). </p>
+	<asp:LoginStatus ID="LoginStatus1" runat="server" />
+</asp:Content>

samples/RelyingPartyPortal/MembersOnly/Default.aspx
@@ -1,19 +1,12 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" %>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-	<title>Welcome OpenID User!</title>
-</head>
-<body>
-	<form id="form1" runat="server">
-	<h1>
+<asp:Content ID="Content1" runat="server" ContentPlaceHolderID="Main">
+	<h2>
 		Members Only Area
-	</h1>
+	</h2>
 	<p>
-		Congratulations, <b>
-			<asp:LoginName ID="LoginName1" runat="server" />
-		</b>. You have completed the OpenID login process.
+		Congratulations, <b><asp:LoginName ID="LoginName1" runat="server" /></b>. 
+		You have completed the OpenID login process.
 	</p>
 	<asp:LoginStatus ID="LoginStatus1" runat="server" />
 
@@ -96,6 +89,4 @@
 			</td>
 		</tr>
 	</table>
-	</form>
-</body>
-</html>
+</asp:Content>

samples/ProviderPortal/ProviderPortal.csproj
@@ -104,6 +104,11 @@
       <Private>True</Private>
     </ProjectReference>
   </ItemGroup>
+  <ItemGroup>
+    <Content Include="images\dotnetopenid_tiny.gif" />
+    <Content Include="Site.Master" />
+    <Content Include="styles.css" />
+  </ItemGroup>
   <Import Project="$(MSBuildBinPath)\Microsoft.CSharp.targets" />
   <Import Project="$(MSBuildExtensionsPath)\Microsoft\VisualStudio\v9.0\WebApplications\Microsoft.WebApplication.targets" />
   <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 

samples/ProviderPortal/Site.Master
@@ -1,0 +1,20 @@
+﻿<%@ Master Language="C#" AutoEventWireup="true" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+	<title>OpenID Provider, by DotNetOpenId</title>
+	<link href="styles.css" rel="stylesheet" type="text/css" />
+	<asp:ContentPlaceHolder ID="head" runat="server" />
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div><a href="http://dotnetopenid.googlecode.com">
+		<img runat="server" src="~/images/dotnetopenid_tiny.gif" title="Jump to the project web site."
+			alt="DotNetOpenId" border='0' /></a> </div>
+	<div>
+		<asp:ContentPlaceHolder ID="Main" runat="server" />
+	</div>
+	</form>
+</body>
+</html>

samples/ProviderPortal/decide.aspx
@@ -1,12 +1,7 @@
-<%@ Page Language="C#" AutoEventWireup="true" Inherits="decide" CodeBehind="decide.aspx.cs" %>
+<%@ Page Language="C#" AutoEventWireup="true" Inherits="decide" CodeBehind="decide.aspx.cs" MasterPageFile="~/Site.Master" %>
 
 <%@ Register Src="ProfileFields.ascx" TagName="ProfileFields" TagPrefix="uc1" %>
-<html>
-<head>
-	<title>Approve OpenID request?</title>
-</head>
-<body>
-	<form id="Form1" runat="server">
+<asp:Content runat="server" ContentPlaceHolderID="Main">
 	<p>
 		A site has asked to authenticate that you own the identifier below.&nbsp; You should 
 		only do this if you wish to log in to the site given by the Realm.</p>
@@ -36,6 +31,4 @@
 	<uc1:ProfileFields ID="profileFields" runat="server" Visible="false" />
 	<asp:Button ID="yes_button" OnClick="Yes_Click" Text="  yes  " runat="Server" />
 	<asp:Button ID="no_button" OnClick="No_Click" Text="  no  " runat="Server" />
-	</form>
-</body>
-</html>
+</asp:Content>

samples/ProviderPortal/decide.aspx.designer.cs
@@ -13,15 +13,6 @@
 public partial class decide {
     
     /// <summary>
-    /// Form1 control.
-    /// </summary>
-    /// <remarks>
-    /// Auto-generated field.
-    /// To modify move field declaration from designer file to code-behind file.
-    /// </remarks>
-    protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
-    
-    /// <summary>
     /// relyingPartyVerificationResultLabel control.
     /// </summary>
     /// <remarks>

commit 12023c3e238c59ea1007adfdd12869bad9be3cb0

Fixed the ProviderPortal sample's directed identity support.
This feature of the sample was accidentally broken in 30a21d9586af74062af9b77728e1595d8e3735cb.

samples/ProviderPortal/Default.aspx
@@ -19,7 +19,11 @@
 	}
 </script>
 
-<asp:Content runat=server ContentPlaceHolderID=Main>
+<asp:Content runat="server" ContentPlaceHolderID="head">
+	<openid:XrdsPublisher runat="server" XrdsUrl="~/op_xrds.aspx" />
+</asp:Content>
+
+<asp:Content runat="server" ContentPlaceHolderID="Main">
 	<h2>
 		Provider
 	</h2>