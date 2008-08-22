b4c99327708a3749e04e602e1bfcc9fbe484cf78
Andrew Arnott <andrewarnott@gmail.com>
2008-08-22 15:23:35 -0700

    Changed RP to use the TracePageAppender so that we can get log messages on godaddy.com.

---
+++ samples/RelyingPartyPortal/Code/TracePageAppender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.IO;
+
+namespace ConsumerPortal.Code {
+	public class TracePageAppender : log4net.Appender.AppenderSkeleton {
+		protected override void Append(log4net.Core.LoggingEvent loggingEvent) {
+			StringWriter sw = new StringWriter(Global.LogMessages);
+			Layout.Format(sw, loggingEvent);
+		}
+	}
+}
