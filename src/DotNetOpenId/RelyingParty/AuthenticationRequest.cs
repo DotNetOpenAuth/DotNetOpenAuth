DotNetOpenAuth/DotNetOpenAuth

commit b60444ac8ad5af8223b3e79aa214dd10c3439a4f

Fixed bug where no association types matching security requirements would generate an ArgumentNullException.
Also added check to detect when OP breaks rules of not changing the association type in the middle of a request. (signon.com does this by responding to a SHA-256 assoc request with a SHA-1 response).

src/DotNetOpenId/RelyingParty/AuthenticationRequest.cs
@@ -175,6 +175,11 @@ namespace DotNetOpenId.RelyingParty {
 
 			if ((assoc == null || !assoc.HasUsefulLifeRemaining) && createNewAssociationIfNeeded) {
 				var req = AssociateRequest.Create(relyingParty, provider);
+				if (req == null) {
+					// this can happen if security requirements and protocol conflict
+					// to where there are no association types to choose from.
+					return null;
+				}
 				if (req.Response != null) {
 					// try again if we failed the first time and have a worthy second-try.
 					if (req.Response.Association == null && req.Response.SecondAttempt != null) {
@@ -182,6 +187,25 @@ namespace DotNetOpenId.RelyingParty {
 						req = req.Response.SecondAttempt;
 					}
 					assoc = req.Response.Association;
+					// Confirm that the association matches the type we requested (section 8.2.1).
+					if (assoc != null) {
+						if (!string.Equals(
+							req.Args[provider.Protocol.openid.assoc_type],
+							req.Response.Args[provider.Protocol.openidnp.assoc_type],
+							StringComparison.Ordinal) ||
+							!string.Equals(
+							req.Args[provider.Protocol.openid.session_type],
+							req.Response.Args[provider.Protocol.openidnp.session_type],
+							StringComparison.Ordinal)) {
+							Logger.ErrorFormat("Provider responded with contradicting association parameters.  Requested [{0}, {1}] but got [{2}, {3}] back.",
+								req.Args[provider.Protocol.openid.assoc_type],
+								req.Args[provider.Protocol.openid.session_type],
+								req.Response.Args[provider.Protocol.openidnp.assoc_type],
+								req.Response.Args[provider.Protocol.openidnp.session_type]);
+
+							assoc = null;
+						}
+					}
 					if (assoc != null) {
 						Logger.InfoFormat("Association with {0} established.", provider.ProviderEndpoint);
 						relyingParty.Store.StoreAssociation(provider.ProviderEndpoint, assoc);

src/DotNetOpenId/RelyingParty/AssociateRequest.cs
@@ -25,10 +25,15 @@ namespace DotNetOpenId.RelyingParty {
 			if (provider == null) throw new ArgumentNullException("provider");
 
 			string assoc_type, session_type;
-			HmacShaAssociation.TryFindBestAssociation(provider.Protocol,
+			if (HmacShaAssociation.TryFindBestAssociation(provider.Protocol,
 				relyingParty.Settings.MinimumHashBitLength, relyingParty.Settings.MaximumHashBitLength,
-				true, out assoc_type, out session_type);
-			return Create(relyingParty, provider, assoc_type, session_type);
+				true, out assoc_type, out session_type)) {
+				return Create(relyingParty, provider, assoc_type, session_type);
+			} else {
+				// There are no associations that meet all requirements.
+				Logger.Warn("Security requirements and protocol combination knock out all possible association types.  Dumb mode forced.");
+				return null;
+			}
 		}
 
 		public static AssociateRequest Create(OpenIdRelyingParty relyingParty, ServiceEndpoint provider, string assoc_type, string session_type) {

src/DotNetOpenId/RelyingParty/DirectRequest.cs
@@ -22,7 +22,7 @@ namespace DotNetOpenId.RelyingParty {
 		}
 		protected ServiceEndpoint Provider { get; private set; }
 		protected Protocol Protocol { get { return Provider.Protocol; } }
-		protected IDictionary<string, string> Args { get; private set; }
+		protected internal IDictionary<string, string> Args { get; private set; }
 		protected OpenIdRelyingParty RelyingParty { get; private set; }
 
 		protected IDictionary<string, string> GetResponse() {

src/DotNetOpenId/RelyingParty/DirectResponse.cs
@@ -36,7 +36,7 @@ namespace DotNetOpenId.RelyingParty {
 		}
 		protected OpenIdRelyingParty RelyingParty { get; private set; }
 		protected ServiceEndpoint Provider { get; private set; }
-		protected IDictionary<string, string> Args { get; private set; }
+		protected internal IDictionary<string, string> Args { get; private set; }
 		protected Protocol Protocol { get { return Provider.Protocol; } }
 	}
 }

commit 281d7031783853b50d235fce13d7b254e4731995

Fixed RP associations with 1.x OPs that broke with commit b60444ac8ad5af8223b3e79aa214dd10c3439a4f.

src/DotNetOpenId/RelyingParty/AuthenticationRequest.cs
@@ -203,13 +203,13 @@ namespace DotNetOpenId.RelyingParty {
 							StringComparison.Ordinal) ||
 							!string.Equals(
 							req.Args[provider.Protocol.openid.session_type],
-							Util.GetRequiredArg(req.Response.Args, provider.Protocol.openidnp.session_type),
+							Util.GetRequiredArgAllowEmptyValue(req.Response.Args, provider.Protocol.openidnp.session_type),
 							StringComparison.Ordinal)) {
 							Logger.ErrorFormat("Provider responded with contradicting association parameters.  Requested [{0}, {1}] but got [{2}, {3}] back.",
 								req.Args[provider.Protocol.openid.assoc_type],
 								req.Args[provider.Protocol.openid.session_type],
 								Util.GetRequiredArg(req.Response.Args, provider.Protocol.openidnp.assoc_type),
-								Util.GetRequiredArg(req.Response.Args, provider.Protocol.openidnp.session_type));
+								Util.GetRequiredArgAllowEmptyValue(req.Response.Args, provider.Protocol.openidnp.session_type));
 
 							assoc = null;
 						}