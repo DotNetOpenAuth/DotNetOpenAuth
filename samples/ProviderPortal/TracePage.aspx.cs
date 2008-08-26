b7495660fb89a707767df9424335a7e413a23a6f
Andrew Arnott <andrewarnott@gmail.com>
2008-08-26 16:45:42 -0700

    Switched to TracePage.aspx on Provider sample as it is on the RelyingParty sample.

---
+++ samples/ProviderPortal/TracePage.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProviderPortal {
+	public partial class TracePage : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			placeHolder1.Controls.Add(new Label { Text = Global.LogMessages.ToString() });
+		}
+
+		protected void clearLogButton_Click(object sender, EventArgs e) {
+			Global.LogMessages.Length = 0;
+			// clear the page immediately, and allow for F5 without a Postback warning.
+			Response.Redirect(Request.Url.AbsoluteUri);
+		}
+	}
+}
