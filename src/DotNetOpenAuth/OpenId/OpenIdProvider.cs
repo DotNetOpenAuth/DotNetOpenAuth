d3ceebcf836253cc28c10418922ccd10efcf7332
Andrew Arnott <andrewarnott@gmail.com>
2008-11-08 21:58:54 -0800

    Added skeletal OpenIdRelyingParty and OpenIdProvider classes.

---
+++ src/DotNetOpenAuth/OpenId/OpenIdProvider.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="OpenIdProvider.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.OpenId {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using DotNetOpenAuth.Messaging;
+	using DotNetOpenAuth.OpenId.ChannelElements;
+
+	/// <summary>
+	/// Offers services for a web page that is acting as an OpenID identity server.
+	/// </summary>
+	public sealed class OpenIdProvider {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OpenIdProvider"/> class.
+		/// </summary>
+		public OpenIdProvider() {
+			this.OpenIdChannel = new OpenIdChannel();
+		}
+
+		/// <summary>
+		/// Gets the channel to use for sending/receiving messages.
+		/// </summary>
+		public Channel Channel {
+			get { return this.OpenIdChannel; }
+		}
+
+		/// <summary>
+		/// Gets or sets the channel to use for sending/receiving messages.
+		/// </summary>
+		internal OpenIdChannel OpenIdChannel { get; set; }
+	}
+}
---
+++ src/DotNetOpenAuth/OpenId/OpenIdRelyingParty.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="OpenIdRelyingParty.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.OpenId {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using DotNetOpenAuth.Messaging;
+	using DotNetOpenAuth.OpenId.ChannelElements;
+
+	/// <summary>
+	/// Provides the programmatic facilities to act as an OpenId consumer.
+	/// </summary>
+	public sealed class OpenIdRelyingParty {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="OpenIdRelyingParty"/> class.
+		/// </summary>
+		public OpenIdRelyingParty() {
+			this.OpenIdChannel = new OpenIdChannel();
+		}
+
+		/// <summary>
+		/// Gets the channel to use for sending/receiving messages.
+		/// </summary>
+		public Channel Channel {
+			get { return this.OpenIdChannel; }
+		}
+
+		/// <summary>
+		/// Gets or sets the channel to use for sending/receiving messages.
+		/// </summary>
+		internal OpenIdChannel OpenIdChannel { get; set; }
+	}
+}
