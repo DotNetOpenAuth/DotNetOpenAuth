78ffced94979a8d020b9300ff4d5cc9663a2aa51
Andrew Arnott <andrewarnott@gmail.com>
2009-03-16 11:01:10 -0700

    Added RP Sreg account creation test.

---
+++ osis/OsisMain/RP/SregAccountCreation.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="RP account creation using Simple Registration" Language="C#" MasterPageFile="~/TestMaster.master"
+	AutoEventWireup="true" CodeFile="SregAccountCreation.aspx.cs" Inherits="RP_SregAccountCreation" %>
+
+<%@ Register Assembly="DotNetOpenAuth" Namespace="DotNetOpenAuth.OpenId.Provider"
+	TagPrefix="op" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="TestHead" runat="Server">
+	<op:IdentityEndpoint ID="IdentityEndpoint1" runat="server" ProviderEndpointUrl="~/RP/SregAccountCreation.aspx"
+		ProviderVersion="V20" />
+	<op:ProviderEndpoint ID="ProviderEndpoint1" runat='server' OnAuthenticationChallenge="ProviderEndpoint1_AuthenticationChallenge" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="TestBody" runat="Server">
+	<asp:Panel runat="server" ID="NoSregRequest" EnableViewState="false" Visible="false">
+		<p>The RP under test sent an authentication request without including the Simple Registration
+			extension. The RP has FAILED this test. </p>
+	</asp:Panel>
+	<h3>Instructions </h3>
+	<ol>
+		<li>Log into an RP using the Claimed Identifier: <asp:Label runat="server" Font-Bold="true"
+			ID="claimedIdLabel" /></li>
+	</ol>
+	<h3>Passing criteria </h3>
+	<p>The RP passes if authentication completes and the RP account creation process has
+		been streamlined using profile values provided by this test, allowing the tester
+		to avoid having to provide personal data such as full name, email address, etc.
+	</p>
+</asp:Content>
---
+++ osis/OsisMain/RP/SregAccountCreation.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
+using DotNetOpenAuth.OpenId.Provider;
+
+public partial class RP_SregAccountCreation : System.Web.UI.Page {
+	protected void Page_Load(object sender, EventArgs e) {
+		if (!IsPostBack) {
+			// Only allow this to be used as an identity page when the test parameter is included
+			IdentityEndpoint1.Visible = !string.IsNullOrEmpty(Request.QueryString["test"]);
+
+			// Set the test parameter that makes each run of the test unique to an RP.
+			claimedIdLabel.Text = DeriveUniqueClaimedIdentifier().AbsoluteUri;
+		}
+	}
+
+	protected void ProviderEndpoint1_AuthenticationChallenge(object sender, AuthenticationChallengeEventArgs e) {
+		ClaimsRequest sregRequest = e.Request.GetExtension<ClaimsRequest>();
+		if (sregRequest == null) {
+			// The RP isn't even sending an sreg extension, so it already failed.
+			NoSregRequest.Visible = true;
+		}
+		e.Request.AddResponseExtension(CreateSregResponse(sregRequest));
+		e.Request.IsAuthenticated = true;
+	}
+
+	private ClaimsResponse CreateSregResponse(ClaimsRequest request) {
+		ClaimsResponse response = request.CreateResponse();
+		if (request.BirthDate != DemandLevel.NoRequest) {
+			response.BirthDate = DateTime.Now - TimeSpan.FromDays(18 * 365);
+		}
+		if (request.Country != DemandLevel.NoRequest) {
+			response.Country = "United States";
+		}
+		if (request.Email != DemandLevel.NoRequest) {
+			response.Email = "osistest@test-id.net";
+		}
+		if (request.FullName != DemandLevel.NoRequest) {
+			response.Country = "OSIS Tester";
+		}
+		if (request.Gender != DemandLevel.NoRequest) {
+			response.Gender = Gender.Male;
+		}
+		if (request.Language != DemandLevel.NoRequest) {
+			response.Language = Thread.CurrentThread.CurrentCulture.Name;
+		}
+		if (request.Nickname != DemandLevel.NoRequest) {
+			response.Nickname = "osistester";
+		}
+		if (request.PostalCode != DemandLevel.NoRequest) {
+			response.PostalCode = "12345";
+		}
+		if (request.TimeZone != DemandLevel.NoRequest) {
+			response.TimeZone = "Pacific time";
+		}
+
+		return response;
+	}
+
+	/// <summary>
+	/// Generates a unique Claimed Identifier for use with this test,
+	/// allowing each tester to get their own claimed_id, guaranteeing
+	/// a valid account creation test since no claimed_id can be used twice.
+	/// </summary>
+	private Uri DeriveUniqueClaimedIdentifier() {
+		UriBuilder claimedId = new UriBuilder(Request.Url);
+		claimedId.Query = null;
+		claimedId.Fragment = null;
+
+		byte[] unique = new byte[6];
+		Random r = new Random();
+		r.NextBytes(unique);
+		claimedId.Query = "test=" + HttpUtility.UrlEncode(unique);
+		return claimedId.Uri;
+	}
+}
