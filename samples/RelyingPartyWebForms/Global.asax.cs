f591de83d8c41b98afaab6359622846f9b465b3a
Andrew Arnott <andrewarnott@gmail.com>
2009-03-15 18:15:39 -0700

    Removed irrelevant "__" prefix handling from samples.

--- samples/RelyingPartyWebForms/Global.asax.cs
+++ samples/RelyingPartyWebForms/Global.asax.cs
@@ -13,9 +13,6 @@
 		public static string ToString(NameValueCollection collection) {
 			using (StringWriter sw = new StringWriter()) {
 				foreach (string key in collection.Keys) {
-					if (key.StartsWith("__")) {
-						continue; // skip
-					}
 					sw.WriteLine("{0} = '{1}'", key, collection[key]);
 				}
 				return sw.ToString();
