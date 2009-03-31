DotNetOpenAuth/DotNetOpenAuth

commit 9ad438261fd4d7bf1a63d0ddbe348c8666919d73

Fixes broken Remember Me checkbox on OpenIdLogin control.
Fixes Google Code Issue 156.

src/DotNetOpenId/RelyingParty/OpenIdTextBox.cs
@@ -207,6 +207,7 @@ namespace DotNetOpenId.RelyingParty
 			set { ViewState[showLogoViewStateKey] = value; }
 		}
 
+		const string usePersistentCookieCallbackKey = "OpenIdTextBox_UsePersistentCookie";
 		const string usePersistentCookieViewStateKey = "UsePersistentCookie";
 		/// <summary>
 		/// Default value of <see cref="UsePersistentCookie"/>.
@@ -555,6 +556,12 @@ namespace DotNetOpenId.RelyingParty
 			if (!Enabled) return;
 			var consumer = new OpenIdRelyingParty();
 			if (consumer.Response != null) {
+				string persistentString = consumer.Response.GetCallbackArgument(usePersistentCookieCallbackKey);
+				bool persistentBool;
+				if (persistentString != null && bool.TryParse(persistentString, out persistentBool)) {
+					UsePersistentCookie = persistentBool;
+				}
+
 				switch (consumer.Response.Status) {
 					case AuthenticationStatus.Canceled:
 						OnCanceled(consumer.Response);
@@ -631,6 +638,9 @@ namespace DotNetOpenId.RelyingParty
 					Request = consumer.CreateRequest(userSuppliedIdentifier, new Realm(realm));
 					Request.Mode = ImmediateMode ? AuthenticationRequestMode.Immediate : AuthenticationRequestMode.Setup;
 					if (EnableRequestProfile) addProfileArgs(Request);
+
+					// Add state that needs to survive across the redirect.
+					Request.AddCallbackArguments(usePersistentCookieCallbackKey, UsePersistentCookie.ToString(CultureInfo.InvariantCulture));
 				} else {
 					Logger.WarnFormat("An invalid identifier was entered ({0}), but not caught by any validation routine.", Text);
 					Request = null;

commit 25954569bfd0fac4655aeb371c6a7bf1a318db0a

Fixed build break caused by 9ad438261fd4d7bf1a63d0ddbe348c8666919d73.

src/DotNetOpenId/RelyingParty/OpenIdTextBox.cs
@@ -556,7 +556,7 @@ namespace DotNetOpenId.RelyingParty
 			if (!Enabled) return;
 			var consumer = new OpenIdRelyingParty();
 			if (consumer.Response != null) {
-				string persistentString = consumer.Response.GetCallbackArgument(usePersistentCookieCallbackKey);
+				string persistentString = Page.Request.QueryString[usePersistentCookieCallbackKey];
 				bool persistentBool;
 				if (persistentString != null && bool.TryParse(persistentString, out persistentBool)) {
 					UsePersistentCookie = persistentBool;