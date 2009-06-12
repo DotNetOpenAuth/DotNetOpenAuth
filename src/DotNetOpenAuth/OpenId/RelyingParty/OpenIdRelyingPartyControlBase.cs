bc0e1952c150871c5c7f12d9d82d5e6cb2c69bfc
Andrew Arnott <andrewarnott@gmail.com>
2009-06-12 02:44:39 -0700

    Fixed UsePersistentCookie persistence in control base class.

--- src/DotNetOpenAuth/OpenId/RelyingParty/OpenIdRelyingPartyControlBase.cs
+++ src/DotNetOpenAuth/OpenId/RelyingParty/OpenIdRelyingPartyControlBase.cs
@@ -400,6 +400,12 @@ namespace DotNetOpenAuth.OpenId.RelyingParty {
 
 			var response = this.RelyingParty.GetResponse();
 			if (response != null) {
+				string persistentString = response.GetCallbackArgument(UsePersistentCookieCallbackKey);
+				bool persistentBool;
+				if (persistentString != null && bool.TryParse(persistentString, out persistentBool)) {
+					this.UsePersistentCookie = persistentBool;
+				}
+
 				switch (response.Status) {
 					case AuthenticationStatus.Authenticated:
 						this.OnLoggedIn(response);
