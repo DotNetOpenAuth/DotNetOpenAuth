0b5fd389ccc28193e0d014b123e23545a1bc5e2d
Andrew Arnott <andrewarnott@gmail.com>
2009-06-16 13:40:55 -0700

    Initial design for the multiple extension interop helper.

---
+++ src/DotNetOpenAuth/OpenId/InteropHelper.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="InteropHelper.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.OpenId {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using DotNetOpenAuth.OpenId;
+
+	public static class InteropHelper {
+		public static void SpreadExtensions(this RelyingParty.IAuthenticationRequest request) {
+			var req = (RelyingParty.AuthenticationRequest)request;
+			throw new NotImplementedException();
+		}
+
+		public static void UnifyExtensions(this RelyingParty.IAuthenticationResponse response) {
+			var resp = (RelyingParty.IAuthenticationResponse)response;
+			throw new NotImplementedException();
+		}
+
+		public static void UnifyExtensions(this Provider.IAuthenticationRequest request) {
+			var req = (Provider.AuthenticationRequest)request;
+			throw new NotImplementedException();
+		}
+	}
+}
