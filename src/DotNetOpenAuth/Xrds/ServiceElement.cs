0f745ae0b1f29886808514e49cb68c3c68810618
Andrew Arnott <andrewarnott@gmail.com>
2009-06-18 07:05:58 -0700

    More mono workarounds for the XPathNavigator bug.

--- src/DotNetOpenAuth/Xrds/ServiceElement.cs
+++ src/DotNetOpenAuth/Xrds/ServiceElement.cs
@@ -48,7 +48,7 @@ namespace DotNetOpenAuth.Xrds {
 			get {
 				List<UriElement> uris = new List<UriElement>();
 				foreach (XPathNavigator node in Node.Select("xrd:URI", XmlNamespaceResolver)) {
-					uris.Add(new UriElement(node, this));
+					uris.Add(new UriElement(node.Clone(), this));
 				}
 				uris.Sort();
 				return uris;
@@ -62,7 +62,7 @@ namespace DotNetOpenAuth.Xrds {
 		public IEnumerable<TypeElement> TypeElements {
 			get {
 				foreach (XPathNavigator node in Node.Select("xrd:Type", XmlNamespaceResolver)) {
-					yield return new TypeElement(node, this);
+					yield return new TypeElement(node.Clone(), this);
 				}
 			}
 		}
