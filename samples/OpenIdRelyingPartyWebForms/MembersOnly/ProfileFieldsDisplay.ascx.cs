20a44edf1bafede354ba873144f7223228987b40
Andrew Arnott <andrewarnott@gmail.com>
2009-06-25 21:31:12 -0700

    Fixed StyleCop messages

--- samples/OpenIdRelyingPartyWebForms/MembersOnly/ProfileFieldsDisplay.ascx.cs
+++ samples/OpenIdRelyingPartyWebForms/MembersOnly/ProfileFieldsDisplay.ascx.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
+﻿namespace OpenIdRelyingPartyWebForms.MembersOnly {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web;
+	using System.Web.UI;
+	using System.Web.UI.WebControls;
+	using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
 
-namespace OpenIdRelyingPartyWebForms.MembersOnly {
 	public partial class ProfileFieldsDisplay : System.Web.UI.UserControl {
 		public ClaimsResponse ProfileValues { get; set; }
 	}
