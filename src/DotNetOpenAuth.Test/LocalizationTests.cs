4ff31bd433a56d6e3bc083e833ca721acaae419e
Andrew Arnott <andrewarnott@gmail.com>
2009-08-29 15:17:01 -0700

    Added localized strings in Serbian.
    Special thanks to Branislav Uzelac who provided the translation.

---
+++ src/DotNetOpenAuth.Test/LocalizationTests.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LocalizationTests.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.Test {
+	using System;
+	using System.Globalization;
+	using System.Threading;
+	using DotNetOpenAuth.Messaging;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	/// <summary>
+	/// Tests various localized resources work as expected.
+	/// </summary>
+	[TestClass]
+	public class LocalizationTests {
+		/// <summary>
+		/// Tests that Serbian localized strings are correctly installed.
+		/// </summary>
+		[TestMethod, ExpectedException(typeof(InvalidOperationException), "Ovaj metod zahteva tekući HttpContext. Kao alternativa, koristite preklopljeni metod koji dozvoljava da se prosledi informacija bez HttpContext-a.")]
+		public void Serbian() {
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("sr");
+			ErrorUtilities.VerifyHttpContext();
+		}
+	}
+}
