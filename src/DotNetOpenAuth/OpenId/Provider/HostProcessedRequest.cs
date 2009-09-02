a93777789e267045e836531fa0b149f6df5a4031
Andrew Arnott <andrewarnott@gmail.com>
2009-09-01 21:06:03 -0700

    Fixed contract build break that only shows up on build machine.

--- src/DotNetOpenAuth/OpenId/Provider/HostProcessedRequest.cs
+++ src/DotNetOpenAuth/OpenId/Provider/HostProcessedRequest.cs
@@ -105,8 +105,6 @@ namespace DotNetOpenAuth.OpenId.Provider {
 		/// See OpenID Authentication 2.0 spec section 9.2.1.
 		/// </remarks>
 		public RelyingPartyDiscoveryResult IsReturnUrlDiscoverable(OpenIdProvider provider) {
-			Contract.Requires<ArgumentNullException>(provider != null);
-
 			if (!this.realmDiscoveryResult.HasValue) {
 				this.realmDiscoveryResult = this.IsReturnUrlDiscoverableCore(provider);
 			}
