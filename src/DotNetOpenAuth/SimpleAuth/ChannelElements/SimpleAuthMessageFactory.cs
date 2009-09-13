DotNetOpenAuth/DotNetOpenAuth

commit 9b20966a35d0614b16763c009902abfe902b50e0

Finished messages for the user agent redirect profile.

src/DotNetOpenAuth/SimpleAuth/ChannelElements/SimpleAuthMessageFactory.cs
@@ -34,19 +34,19 @@ using DotNetOpenAuth.SimpleAuth.Messages;
 		/// A newly instantiated <see cref="IProtocolMessage"/>-derived object that this message can
 		/// deserialize to.  Null if the request isn't recognized as a valid protocol message.
 		/// </returns>
-		public IDirectedProtocolMessage GetNewRequestMessage(MessageReceivingEndpoint recipient, IDictionary<string, string> fields) {
+		public IDirectedProtocolMessage GetNewRequestMessage(Uri recipient, IDictionary<string, string> fields) {
 			Version version = Protocol.DefaultVersion;
 
-			if (fields.ContainsKey("sa_consumer_key") && fields.ContainsKey("sa_callback")) {
+			if (fields.ContainsKey(Protocol.sa_consumer_key) && fields.ContainsKey(Protocol.sa_callback)) {
 				return new UserAuthorizationInUserAgentRequest(recipient, version);
 			}
 
-			if (fields.ContainsKey("sa_consumer_key") && fields.ContainsKey("sa_verifier")) {
-				return new RequestAccessTokenWithVerifier(recipient.Location, version);
+			if (fields.ContainsKey(Protocol.sa_consumer_key) && fields.ContainsKey(Protocol.sa_verifier)) {
+				return new RequestAccessTokenWithVerifier(recipient, version);
 			}
 
-			if (fields.ContainsKey("sa_verifier")) {
-				return new UserAuthorizationInUserAgentResponse(recipient.Location, version);
+			if (fields.ContainsKey(Protocol.sa_verifier)) {
+				return new UserAuthorizationInUserAgentGrantedResponse(recipient, version);
 			}
 
 			return null;

src/DotNetOpenAuth/DotNetOpenAuth.csproj
@@ -380,7 +380,7 @@
     <Compile Include="OpenId\Extensions\ProviderAuthenticationPolicy\PapeUtilities.cs" />
     <Compile Include="OpenId\Extensions\ProviderAuthenticationPolicy\PolicyRequest.cs" />
     <Compile Include="OpenId\Extensions\ProviderAuthenticationPolicy\PolicyResponse.cs" />
-    <Compile Include="OpenId\Extensions\ProviderAuthenticationPolicy\TimespanSecondsEncoder.cs" />
+    <Compile Include="Messaging\TimespanSecondsEncoder.cs" />
     <Compile Include="OpenId\Extensions\SimpleRegistration\ClaimsRequest.cs" />
     <Compile Include="OpenId\Extensions\SimpleRegistration\ClaimsResponse.cs" />
     <Compile Include="OpenId\Extensions\SimpleRegistration\Constants.cs" />
@@ -510,9 +510,13 @@
     <Compile Include="Messaging\MessageReceivingEndpoint.cs" />
     <Compile Include="SimpleAuth\ChannelElements\SimpleAuthChannel.cs" />
     <Compile Include="SimpleAuth\ChannelElements\SimpleAuthMessageFactory.cs" />
+    <Compile Include="SimpleAuth\ChannelElements\UriOrOutOfBandEncoding.cs" />
+    <Compile Include="SimpleAuth\Messages\AccessTokenResponse.cs" />
+    <Compile Include="SimpleAuth\Messages\MessageBase.cs" />
     <Compile Include="SimpleAuth\Messages\RequestAccessTokenWithVerifier.cs" />
+    <Compile Include="SimpleAuth\Messages\UserAuthorizationInUserAgentDeniedResponse.cs" />
     <Compile Include="SimpleAuth\Messages\UserAuthorizationInUserAgentRequest.cs" />
-    <Compile Include="SimpleAuth\Messages\UserAuthorizationInUserAgentResponse.cs" />
+    <Compile Include="SimpleAuth\Messages\UserAuthorizationInUserAgentGrantedResponse.cs" />
     <Compile Include="SimpleAuth\Protocol.cs" />
     <Compile Include="SimpleAuth\SimpleAuthStrings.Designer.cs">
       <AutoGen>True</AutoGen>

src/DotNetOpenAuth/Messaging/TimespanSecondsEncoder.cs
@@ -4,7 +4,7 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-namespace DotNetOpenAuth.OpenId.Extensions.ProviderAuthenticationPolicy {
+namespace DotNetOpenAuth.Messaging {
 	using System;
 	using System.Globalization;
 	using DotNetOpenAuth.Messaging.Reflection;
@@ -13,6 +13,12 @@ namespace DotNetOpenAuth.OpenId.Extensions.ProviderAuthenticationPolicy {
 	/// Encodes and decodes the <see cref="TimeSpan"/> as an integer of total seconds.
 	/// </summary>
 	internal class TimespanSecondsEncoder : IMessagePartEncoder {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TimespanSecondsEncoder"/> class.
+		/// </summary>
+		internal TimespanSecondsEncoder() {
+		}
+
 		#region IMessagePartEncoder Members
 
 		/// <summary>

src/DotNetOpenAuth/SimpleAuth/ChannelElements/UriOrOutOfBandEncoding.cs
@@ -1,0 +1,83 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UriOrOutOfBandEncoding.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.SimpleAuth.ChannelElements {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using DotNetOpenAuth.Messaging;
+	using DotNetOpenAuth.Messaging.Reflection;
+
+	/// <summary>
+	/// An URI encoder that translates null <see cref="Uri"/> references as "out_of_band" 
+	/// instead of an empty/missing argument.
+	/// </summary>
+	/// <remarks>
+	/// This class is functionality equivalent to the <see cref="DotNetOpenAuth.OAuth.UriOrOobEncoding"/>
+	/// encoding element, except that instead of using "oob" for null Uri values,
+	/// "out_of_band" is used.
+	/// </remarks>
+	internal class UriOrOutOfBandEncoding : IMessagePartNullEncoder {
+		/// <summary>
+		/// The string constant "oob", used to indicate an out-of-band configuration.
+		/// </summary>
+		private const string OutOfBandConfiguration = "out_of_band";
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UriOrOutOfBandEncoding"/> class.
+		/// </summary>
+		public UriOrOutOfBandEncoding() {
+		}
+
+		#region IMessagePartNullEncoder Members
+
+		/// <summary>
+		/// Gets the string representation to include in a serialized message
+		/// when the message part has a <c>null</c> value.
+		/// </summary>
+		/// <value></value>
+		public string EncodedNullValue {
+			get { return OutOfBandConfiguration; }
+		}
+
+		#endregion
+
+		#region IMessagePartEncoder Members
+
+		/// <summary>
+		/// Encodes the specified value.
+		/// </summary>
+		/// <param name="value">The value.  Guaranteed to never be null.</param>
+		/// <returns>
+		/// The <paramref name="value"/> in string form, ready for message transport.
+		/// </returns>
+		public string Encode(object value) {
+			ErrorUtilities.VerifyArgumentNotNull(value, "value");
+
+			Uri uriValue = (Uri)value;
+			return uriValue.AbsoluteUri;
+		}
+
+		/// <summary>
+		/// Decodes the specified value.
+		/// </summary>
+		/// <param name="value">The string value carried by the transport.  Guaranteed to never be null, although it may be empty.</param>
+		/// <returns>
+		/// The deserialized form of the given string.
+		/// </returns>
+		/// <exception cref="FormatException">Thrown when the string value given cannot be decoded into the required object type.</exception>
+		public object Decode(string value) {
+			if (string.Equals(value, OutOfBandConfiguration, StringComparison.Ordinal)) {
+				return null;
+			} else {
+				return new Uri(value, UriKind.Absolute);
+			}
+		}
+
+		#endregion
+	}
+}

src/DotNetOpenAuth/SimpleAuth/Messages/AccessTokenResponse.cs
@@ -1,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AccessTokenResponse.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.SimpleAuth.Messages {
+	using System;
+	using DotNetOpenAuth.Messaging;
+
+	/// <summary>
+	/// The direct response message that contains the access token from the Token Issuer
+	/// to the Consumer.
+	/// </summary>
+	internal class AccessTokenResponse : MessageBase {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AccessTokenResponse"/> class.
+		/// </summary>
+		/// <param name="version">The version.</param>
+		internal AccessTokenResponse(Version version)
+			: base(version) {
+		}
+
+		/// <summary>
+		/// Gets or sets the access token.
+		/// </summary>
+		/// <value>The token.</value>
+		[MessagePart(Protocol.sa_token, IsRequired = true, AllowEmpty = false)]
+		internal string Token { get; set; }
+
+		/// <summary>
+		/// Gets or sets the lifetime of the access token.
+		/// </summary>
+		/// <value>The lifetime.</value>
+		[MessagePart(Protocol.sa_token_expires_in, IsRequired = false, AllowEmpty = false, Encoder = typeof(TimespanSecondsEncoder))]
+		internal TimeSpan? Lifetime { get; set; }
+	}
+}

src/DotNetOpenAuth/SimpleAuth/Messages/MessageBase.cs
@@ -1,0 +1,177 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MessageBase.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.SimpleAuth.Messages {
+	using System;
+	using System.Collections.Generic;
+	using DotNetOpenAuth.Messaging;
+
+	/// <summary>
+	/// A common message base class for Simple Auth messages.
+	/// </summary>
+	public class MessageBase : IDirectedProtocolMessage {
+		/// <summary>
+		/// A dictionary to contain extra message data.
+		/// </summary>
+		private Dictionary<string, string> extraData = new Dictionary<string, string>();
+
+		/// <summary>
+		/// The backing field for the <see cref="IMessage.Version"/> property.
+		/// </summary>
+		private Version version;
+
+		/// <summary>
+		/// A value indicating whether this message is a direct or indirect message.
+		/// </summary>
+		private MessageTransport messageTransport;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MessageBase"/> class
+		/// that is used for direct response messages.
+		/// </summary>
+		/// <param name="version">The version.</param>
+		protected MessageBase(Version version) {
+			ErrorUtilities.VerifyArgumentNotNull(version, "version");
+			this.messageTransport = MessageTransport.Direct;
+			this.version = version;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MessageBase"/> class
+		/// that is used for directed messages.
+		/// </summary>
+		/// <param name="version">The version.</param>
+		/// <param name="messageTransport">The message transport.</param>
+		/// <param name="recipient">The recipient.</param>
+		protected MessageBase(Version version, MessageTransport messageTransport, Uri recipient) {
+			ErrorUtilities.VerifyArgumentNotNull(version, "version");
+			ErrorUtilities.VerifyArgumentNotNull(recipient, "recipient");
+
+			this.version = version;
+			this.messageTransport = messageTransport;
+			this.Recipient = recipient;
+			this.HttpMethods = HttpDeliveryMethods.GetRequest | HttpDeliveryMethods.PostRequest;
+		}
+
+		#region IMessage Properties
+
+		/// <summary>
+		/// Gets the version of the protocol or extension this message is prepared to implement.
+		/// </summary>
+		/// <remarks>
+		/// Implementations of this interface should ensure that this property never returns null.
+		/// </remarks>
+		Version IMessage.Version {
+			get { return this.version; }
+		}
+
+		/// <summary>
+		/// Gets the extra, non-standard Protocol parameters included in the message.
+		/// </summary>
+		/// <value></value>
+		/// <remarks>
+		/// Implementations of this interface should ensure that this property never returns null.
+		/// </remarks>
+		public IDictionary<string, string> ExtraData {
+			get { return this.extraData; }
+		}
+
+		#endregion
+
+		#region IProtocolMessage Members
+
+		/// <summary>
+		/// Gets the level of protection this message requires.
+		/// </summary>
+		/// <value><see cref="MessageProtections.None"/></value>
+		MessageProtections IProtocolMessage.RequiredProtection {
+			get { return MessageProtections.None; }
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this is a direct or indirect message.
+		/// </summary>
+		/// <value></value>
+		MessageTransport IProtocolMessage.Transport {
+			get { return this.messageTransport; }
+		}
+
+		#endregion
+
+		#region IDirectedProtocolMessage Members
+
+		/// <summary>
+		/// Gets the preferred method of transport for the message.
+		/// </summary>
+		/// <remarks>
+		/// For indirect messages this will likely be GET+POST, which both can be simulated in the user agent:
+		/// the GET with a simple 301 Redirect, and the POST with an HTML form in the response with javascript
+		/// to automate submission.
+		/// </remarks>
+		HttpDeliveryMethods IDirectedProtocolMessage.HttpMethods {
+			get { return this.HttpMethods; }
+		}
+
+		/// <summary>
+		/// Gets the URL of the intended receiver of this message.
+		/// </summary>
+		Uri IDirectedProtocolMessage.Recipient {
+			get { return this.Recipient; }
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Gets or sets the preferred method of transport for the message.
+		/// </summary>
+		/// <remarks>
+		/// For indirect messages this will likely be GET+POST, which both can be simulated in the user agent:
+		/// the GET with a simple 301 Redirect, and the POST with an HTML form in the response with javascript
+		/// to automate submission.
+		/// </remarks>
+		protected HttpDeliveryMethods HttpMethods { get; set; }
+
+		/// <summary>
+		/// Gets the URL of the intended receiver of this message.
+		/// </summary>
+		protected Uri Recipient { get; private set; }
+
+		#region IMessage Methods
+
+		/// <summary>
+		/// Checks the message state for conformity to the protocol specification
+		/// and throws an exception if the message is invalid.
+		/// </summary>
+		/// <remarks>
+		/// 	<para>Some messages have required fields, or combinations of fields that must relate to each other
+		/// in specialized ways.  After deserializing a message, this method checks the state of the
+		/// message to see if it conforms to the protocol.</para>
+		/// 	<para>Note that this property should <i>not</i> check signatures or perform any state checks
+		/// outside this scope of this particular message.</para>
+		/// </remarks>
+		/// <exception cref="ProtocolException">Thrown if the message is invalid.</exception>
+		void IMessage.EnsureValidMessage() {
+			this.EnsureValidMessage();
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Checks the message state for conformity to the protocol specification
+		/// and throws an exception if the message is invalid.
+		/// </summary>
+		/// <remarks>
+		/// 	<para>Some messages have required fields, or combinations of fields that must relate to each other
+		/// in specialized ways.  After deserializing a message, this method checks the state of the
+		/// message to see if it conforms to the protocol.</para>
+		/// 	<para>Note that this property should <i>not</i> check signatures or perform any state checks
+		/// outside this scope of this particular message.</para>
+		/// </remarks>
+		/// <exception cref="ProtocolException">Thrown if the message is invalid.</exception>
+		protected virtual void EnsureValidMessage() {
+		}
+	}
+}

src/DotNetOpenAuth/SimpleAuth/Messages/RequestAccessTokenWithVerifier.cs
@@ -6,99 +6,58 @@
 
 namespace DotNetOpenAuth.SimpleAuth.Messages {
 	using System;
-	using System.Collections.Generic;
-	using System.Diagnostics.Contracts;
-	using System.Linq;
-	using System.Text;
 	using DotNetOpenAuth.Messaging;
+	using DotNetOpenAuth.SimpleAuth.ChannelElements;
 
 	/// <summary>
 	/// A message sent by the Consumer directly to the Token Issuer to exchange
 	/// the verifier code for an Access Token.
 	/// </summary>
-	internal class RequestAccessTokenWithVerifier : IDirectedProtocolMessage {
-		/// <summary>
-		/// A dictionary to contain extra message data.
-		/// </summary>
-		private Dictionary<string, string> extraData = new Dictionary<string, string>();
-
+	internal class RequestAccessTokenWithVerifier : MessageBase, IDirectedProtocolMessage {
 		/// <summary>
 		/// Initializes a new instance of the <see cref="RequestAccessTokenWithVerifier"/> class.
 		/// </summary>
 		/// <param name="tokenIssuer">The token issuer.</param>
 		/// <param name="version">The version.</param>
-		internal RequestAccessTokenWithVerifier(Uri tokenIssuer, Version version) {
-			Contract.Requires<ArgumentNullException>(tokenIssuer != null);
-			Contract.Requires<ArgumentNullException>(version != null);
-
-			this.Recipient = tokenIssuer;
-			this.Version = version;
+		internal RequestAccessTokenWithVerifier(Uri tokenIssuer, Version version)
+			: base(version, MessageTransport.Direct, tokenIssuer) {
+			this.HttpMethods = HttpDeliveryMethods.PostRequest;
 		}
 
-		#region IDirectedProtocolMessage Members
+		/// <summary>
+		/// Gets or sets the consumer key.
+		/// </summary>
+		/// <value>The consumer key.</value>
+		[MessagePart(Protocol.sa_consumer_key, IsRequired = true, AllowEmpty = false)]
+		internal string ConsumerKey { get; set; }
 
 		/// <summary>
-		/// Gets the preferred method of transport for the message.
+		/// Gets or sets the consumer secret.
 		/// </summary>
-		/// <value><see cref="HttpDeliveryMethods.PostRequest"/></value>
+		/// <value>The consumer secret.</value>
+		[MessagePart(Protocol.sa_consumer_secret, IsRequired = true, AllowEmpty = false)]
+		internal string ConsumerSecret { get; set; }
+
+		/// <summary>
+		/// Gets or sets the verifier.
+		/// </summary>
+		/// <value>The verifier.</value>
+		[MessagePart(Protocol.sa_verifier, IsRequired = true, AllowEmpty = false)]
+		internal string Verifier { get; set; }
+
+		/// <summary>
+		/// Gets or sets the callback URL.
+		/// </summary>
+		/// <value>
+		/// An absolute URL to which the Token Issuer will redirect the User back after
+		/// the user has approved the authorization request.
+		/// </value>
 		/// <remarks>
-		/// For indirect messages this will likely be GET+POST, which both can be simulated in the user agent:
-		/// the GET with a simple 301 Redirect, and the POST with an HTML form in the response with javascript
-		/// to automate submission.
+		/// Consumers which are unable to receive callbacks MUST use <c>null</c> to indicate it
+		/// will receive the Verification Code out of band.
 		/// </remarks>
-		public HttpDeliveryMethods HttpMethods {
-			get { return HttpDeliveryMethods.PostRequest; }
-		}
-
-		/// <summary>
-		/// Gets the URL of the intended receiver of this message.
-		/// </summary>
-		public Uri Recipient { get; private set; }
-
-		#endregion
-
-		#region IProtocolMessage Members
-
-		/// <summary>
-		/// Gets the level of protection this message requires.
-		/// </summary>
-		/// <value><see cref="MessageProtections.TamperProtection"/></value>
-		/// <remarks>
-		/// The protection this message requires is provided by the mandated use of HTTPS.
-		/// </remarks>
-		public MessageProtections RequiredProtection {
-			get { return MessageProtections.TamperProtection; }
-		}
-
-		/// <summary>
-		/// Gets a value indicating whether this is a direct or indirect message.
-		/// </summary>
-		/// <value><see cref="MessageTransport.Direct"/></value>
-		public MessageTransport Transport {
-			get { return MessageTransport.Direct; }
-		}
-
-		#endregion
-
-		#region IMessage Members
-
-		/// <summary>
-		/// Gets the version of the protocol or extension this message is prepared to implement.
-		/// </summary>
-		/// <remarks>
-		/// Implementations of this interface should ensure that this property never returns null.
-		/// </remarks>
-		public Version Version { get; private set; }
-
-		/// <summary>
-		/// Gets the extra, non-standard Protocol parameters included in the message.
-		/// </summary>
-		/// <remarks>
-		/// Implementations of this interface should ensure that this property never returns null.
-		/// </remarks>
-		public IDictionary<string, string> ExtraData {
-			get { return this.extraData; }
-		}
+		[MessagePart(Protocol.sa_callback, IsRequired = true, AllowEmpty = false, Encoder = typeof(UriOrOutOfBandEncoding))]
+		internal Uri Callback { get; set; }
 
 		/// <summary>
 		/// Checks the message state for conformity to the protocol specification
@@ -112,10 +71,9 @@ namespace DotNetOpenAuth.SimpleAuth.Messages {
 		/// outside this scope of this particular message.</para>
 		/// </remarks>
 		/// <exception cref="ProtocolException">Thrown if the message is invalid.</exception>
-		public void EnsureValidMessage() {
+		protected override void EnsureValidMessage() {
+			base.EnsureValidMessage();
 			ErrorUtilities.VerifyProtocol(this.Recipient.IsTransportSecure(), SimpleAuthStrings.HttpsRequired);
 		}
-
-		#endregion
 	}
 }

commit 1ddcf8cc722d4c6b6e92151296ba657372dd55d8

Fixed build break introduced in 9b20966a35d0614b16763c009902abfe902b50e0.

src/DotNetOpenAuth/SimpleAuth/ChannelElements/SimpleAuthMessageFactory.cs
@@ -34,19 +34,19 @@ using DotNetOpenAuth.SimpleAuth.Messages;
 		/// A newly instantiated <see cref="IProtocolMessage"/>-derived object that this message can
 		/// deserialize to.  Null if the request isn't recognized as a valid protocol message.
 		/// </returns>
-		public IDirectedProtocolMessage GetNewRequestMessage(Uri recipient, IDictionary<string, string> fields) {
+		public IDirectedProtocolMessage GetNewRequestMessage(MessageReceivingEndpoint recipient, IDictionary<string, string> fields) {
 			Version version = Protocol.DefaultVersion;
 
 			if (fields.ContainsKey(Protocol.sa_consumer_key) && fields.ContainsKey(Protocol.sa_callback)) {
-				return new UserAuthorizationInUserAgentRequest(recipient, version);
+				return new UserAuthorizationInUserAgentRequest(recipient.Location, version);
 			}
 
 			if (fields.ContainsKey(Protocol.sa_consumer_key) && fields.ContainsKey(Protocol.sa_verifier)) {
-				return new RequestAccessTokenWithVerifier(recipient, version);
+				return new RequestAccessTokenWithVerifier(recipient.Location, version);
 			}
 
 			if (fields.ContainsKey(Protocol.sa_verifier)) {
-				return new UserAuthorizationInUserAgentGrantedResponse(recipient, version);
+				return new UserAuthorizationInUserAgentGrantedResponse(recipient.Location, version);
 			}
 
 			return null;