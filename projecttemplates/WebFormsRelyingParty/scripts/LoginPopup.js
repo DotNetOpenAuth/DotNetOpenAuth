66793b18feaf89ba7c5bf1299fe0a6ef2c0a4088
Andrew Arnott <andrewarnott@gmail.com>
2009-10-17 20:13:38 -0700

    Preparing for the behavior where sending the positive assertion to the RP and logging in will actually happen.

--- projecttemplates/WebFormsRelyingParty/scripts/LoginPopup.js
+++ projecttemplates/WebFormsRelyingParty/scripts/LoginPopup.js
@@ -31,19 +31,21 @@
 		}
 	}
 
-	function doLogin(identifier, timerBased) {
+	function checkidSetup(identifier, timerBased) {
 		var retain = !$('#NotMyComputer')[0].selected;
 		$.cookie('openid_identifier', retain ? identifier : null, { path: '/' });
 		var openid = new window.OpenIdIdentifier(identifier);
-		if (openid) {
-			openid.login(function(discoveryResult, respondingEndpoint, extensionResponses) {
-				showLoginSuccess(discoveryResult.userSuppliedIdentifier);
-				//alert("Woot!  We've logged you in as " + respondingEndpoint.claimedIdentifier);
-				//window.postLoginAssertion(respondingEndpoint.response, window.parent.location.href);
-			});
-		} else {
-			trace('doLogin called without an identifier.');
-		}
+		if (!openid) { throw 'checkidSetup called without an identifier.'; }
+		openid.login(function(discoveryResult, respondingEndpoint, extensionResponses) {
+			showLoginSuccess(discoveryResult.userSuppliedIdentifier);
+			doLogin(respondingEndpoint.claimedIdentifier);
+		});
+	}
+
+	// Sends the positive assertion we've collected to the server and actually logs the user into the RP.
+	function doLogin(identifier) {
+		alert('at this point, the whole page would refresh and you would be logged in as ' + identifier);
+		//window.postLoginAssertion(respondingEndpoint.response, window.parent.location.href);
 	}
 
 	// This FrameManager will be used for background logins for the OP buttons
@@ -76,11 +78,17 @@
 		if ($(this).hasClass('grayedOut')) {
 			$('ul.OpenIdProviders li').removeClass('grayedOut');
 		}
-		
-		// Be sure to hide the openid_identifier text box unless the OpenID button is selected.
-		if ($(this)[0] != $('#OpenIDButton')[0]) {
-			$('#OpenIDForm').hide('slow');
+
+		// If the user clicked on a button that has the "we're ready to log you in immediately",
+		// then log them in!
+		if ($(this).hasClass('loginSuccess')) {
 			doLogin($(this)[0].id);
+		} else {
+			// Be sure to hide the openid_identifier text box unless the OpenID button is selected.
+			if ($(this)[0] != $('#OpenIDButton')[0]) {
+				$('#OpenIDForm').hide('slow');
+				checkidSetup($(this)[0].id);
+			} 
 		}
 	});
 	$('#OpenIDButton').click(function() {
