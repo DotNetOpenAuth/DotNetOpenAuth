70a8f7e3d132a194be6100f2544055de13995bf0
Andrew Arnott <andrewarnott@gmail.com>
2009-10-20 06:27:28 -0700

    Integrated login page so it actually has the behavior in the site it should for the full page redirect scenario.
    Still missing InfoCard.

--- projecttemplates/WebFormsRelyingParty/LoginFrame.aspx.cs
+++ projecttemplates/WebFormsRelyingParty/LoginFrame.aspx.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Globalization;
-using System.Web.Security;
 using DotNetOpenAuth.Messaging;
+using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
 using DotNetOpenAuth.OpenId.RelyingParty;
 
 namespace WebFormsRelyingParty {
@@ -15,7 +16,44 @@ namespace WebFormsRelyingParty {
 		}
 
 		protected void openIdButtonPanel_LoggedIn(object sender, OpenIdEventArgs e) {
-			FormsAuthentication.RedirectFromLoginPage(e.ClaimedIdentifier, false);
+			this.LoginUser(e.ClaimedIdentifier, e.Response.FriendlyIdentifierForDisplay, e.Response.GetExtension<ClaimsResponse>());
+		}
+
+		protected void InfoCardSelector1_ReceivedToken(object sender, DotNetOpenAuth.InfoCard.ReceivedTokenEventArgs e) {
+			this.LoginUser(AuthenticationToken.SynthesizeClaimedIdentifierFromInfoCard(e.Token.UniqueId), e.Token.SiteSpecificId, null);
+		}
+
+		private void LoginUser(string claimedIdentifier, string friendlyIdentifier, ClaimsResponse claims) {
+			// Create an account for this user if we don't already have one.
+			AuthenticationToken openidToken = Global.DataContext.AuthenticationToken.FirstOrDefault(token => token.ClaimedIdentifier == claimedIdentifier);
+			if (openidToken == null) {
+				// this is a user we haven't seen before.
+				User user = new User();
+				openidToken = new AuthenticationToken {
+					ClaimedIdentifier = claimedIdentifier,
+					FriendlyIdentifier = friendlyIdentifier,
+				};
+				user.AuthenticationTokens.Add(openidToken);
+
+				// Gather information about the user if it's available.
+				if (claims != null) {
+					if (!string.IsNullOrEmpty(claims.Email)) {
+						user.EmailAddress = claims.Email;
+					}
+					if (!string.IsNullOrEmpty(claims.FullName)) {
+						if (claims.FullName.IndexOf(' ') > 0) {
+							user.FirstName = claims.FullName.Substring(0, claims.FullName.IndexOf(' ')).Trim();
+							user.LastName = claims.FullName.Substring(claims.FullName.IndexOf(' ')).Trim();
+						} else {
+							user.FirstName = claims.FullName;
+						}
+					}
+				}
+
+				Global.DataContext.AddToUser(user);
+			}
+
+			FormsAuthentication.RedirectFromLoginPage(openidToken.ClaimedIdentifier, false);
 		}
 	}
 }
