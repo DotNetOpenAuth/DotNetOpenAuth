DotNetOpenAuth/DotNetOpenAuth

commit 8576d3e52d930d4a784ba26ecfba9b79ecef2076

Fixed calculation of web root in the OAuth SP sample.

samples/OAuthServiceProvider/App_Code/Global.cs
@@ -92,7 +92,11 @@ public class Global : HttpApplication {
 	private void Application_Start(object sender, EventArgs e) {
 		log4net.Config.XmlConfigurator.Configure();
 		Logger.Info("Sample starting...");
-		Constants.WebRootUrl = new Uri(HttpContext.Current.Request.Url, "/");
+		string appPath = HttpContext.Current.Request.ApplicationPath;
+		if (!appPath.EndsWith("/")) {
+			appPath += "/";
+		}
+		Constants.WebRootUrl = new Uri(appPath);
 		var tokenManager = new DatabaseTokenManager();
 		Global.TokenManager = tokenManager;
 	}

commit 5e21bc06b16e6c4bed2a9c29294b1203bd92bd3c

Fixed regression introduced in 8576d3e52d9 and added comment regarding how it's broken in some IIS configurations.

samples/OAuthServiceProvider/App_Code/Global.cs
@@ -96,7 +96,10 @@ public class Global : HttpApplication {
 		if (!appPath.EndsWith("/")) {
 			appPath += "/";
 		}
-		Constants.WebRootUrl = new Uri(appPath);
+		// This will break in IIS Integrated Pipeline mode, since applications
+		// start before the first incoming request context is available.
+		// TODO: fix this.
+		Constants.WebRootUrl = new Uri(HttpContext.Current.Request.Url, appPath);
 		var tokenManager = new DatabaseTokenManager();
 		Global.TokenManager = tokenManager;
 	}