9eda89ad1ff263dd51af0468ab5dca6ace3b898e
Andrew Arnott <andrewarnott@gmail.com>
2009-12-02 21:33:03 -0800

    Added a db project for the project template db schema.

---
+++ projecttemplates/RelyingPartyDatabase/Schema Objects/Database Level Objects/Storage/Files/C__Users_andarno_git_dotnetopenid_projecttemplates_WebFormsRelyingParty_App_Data_Database.mdf.sqlfile.sql
@@ -0,0 +1,3 @@
+﻿ALTER DATABASE [$(DatabaseName)]
+    ADD FILE (NAME = [C:\Users\andarno\git\dotnetopenid\projecttemplates\WebFormsRelyingParty\App_Data\Database.mdf], FILENAME = '$(Path2)Database.mdf', MAXSIZE = UNLIMITED, FILEGROWTH = 1024 KB) TO FILEGROUP [PRIMARY];
+
---
+++ projecttemplates/RelyingPartyDatabase/Schema Objects/Database Level Objects/Storage/Files/Database_log.sqlfile.sql
@@ -0,0 +1,3 @@
+﻿ALTER DATABASE [$(DatabaseName)]
+    ADD LOG FILE (NAME = [Database_log], FILENAME = '$(Path1)Database_log.LDF', MAXSIZE = 2097152 MB, FILEGROWTH = 10 %);
+
