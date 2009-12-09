3a79bf93105e966a7b76baa2da8c864ba3f9d4c3
Andrew Arnott <andrewarnott@gmail.com>
2009-12-09 11:24:53 -0800

    Fixed db proj schema for project template as well.

--- projecttemplates/RelyingPartyDatabase/Schema Objects/Schemas/dbo/Tables/Consumer.table.sql
+++ projecttemplates/RelyingPartyDatabase/Schema Objects/Schemas/dbo/Tables/Consumer.table.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Consumer] (
     [ConsumerId]             INT             IDENTITY (1, 1) NOT NULL,
     [ConsumerKey]            NVARCHAR (255)  COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
-    [ConsumerSecret]         NVARCHAR (255)  NULL,
+    [ConsumerSecret]         NVARCHAR (255)  COLLATE SQL_Latin1_General_CP1_CS_AS NULL,
     [X509Certificate]        IMAGE           NULL,
     [Callback]               NVARCHAR (2048) NULL,
     [VerificationCodeFormat] INT             NOT NULL,
--- projecttemplates/RelyingPartyDatabase/Schema Objects/Schemas/dbo/Tables/IssuedToken.table.sql
+++ projecttemplates/RelyingPartyDatabase/Schema Objects/Schemas/dbo/Tables/IssuedToken.table.sql
@@ -6,7 +6,7 @@
     [TokenSecret]      NVARCHAR (255)  COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
     [CreatedOn]        DATETIME        NOT NULL,
     [Callback]         NVARCHAR (2048) NULL,
-    [VerificationCode] NVARCHAR (255)  NULL,
+    [VerificationCode] NVARCHAR (255)  COLLATE SQL_Latin1_General_CP1_CS_AS NULL,
     [ConsumerVersion]  VARCHAR (10)    NULL,
     [ExpirationDate]   DATETIME        NULL,
     [IsAccessToken]    BIT             NOT NULL,
