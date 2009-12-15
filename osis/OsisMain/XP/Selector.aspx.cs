952b09fd99fd53e76c2693e070d1f43659905fce
Andrew Arnott <andrewarnott@gmail.com>
2009-12-15 10:18:50 -0700

    Added Microsoft's multi-protocol selector tests.

---
+++ osis/OsisMain/XP/Selector.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
+using DotNetOpenAuth.OpenId.RelyingParty;
+
+public partial class XP_Selector : System.Web.UI.Page {
+	private OpenIdRelyingParty rp = new OpenIdRelyingParty();
+	protected void Page_Load(object sender, EventArgs e) {
+		var response = rp.GetResponse();
+		if (response != null) {
+			if (response.Status == AuthenticationStatus.Authenticated) {
+				MultiView1.SetActiveView(View2);
+			} else {
+				MultiView1.SetActiveView(View3);
+				errorDetails.Text = HttpUtility.HtmlEncode(response.Exception.Message);
+			}
+		}
+	}
+}
