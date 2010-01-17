8d4c1269e7bc536ec4fb1209bfa5fcc1a9443cf6
Andrew Arnott <andrewarnott@gmail.com>
2010-01-17 08:53:56 -0800

    Added OpenIdRelyingPartyWebFormsVB sample.

---
+++ samples/OpenIdRelyingPartyWebFormsVB/Code/TracePageAppender.vb
@@ -0,0 +1,14 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Web
+
+
+Public Class TracePageAppender
+	Inherits log4net.Appender.AppenderSkeleton
+
+	Protected Overrides Sub Append(ByVal loggingEvent As log4net.Core.LoggingEvent)
+		Dim sw As StringWriter = New StringWriter(Global_asax.LogMessages)
+		Layout.Format(sw, loggingEvent)
+	End Sub
+End Class
---
+++ samples/OpenIdRelyingPartyWebFormsVB/TracePage.aspx.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Public Class TracePage
+	Inherits System.Web.UI.Page
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Me.placeHolder1.Controls.Add(New Label() With {.Text = HttpUtility.HtmlEncode(Global_asax.LogMessages.ToString())})
+	End Sub
+
+	Protected Sub clearLogButton_Click(ByVal sender As Object, ByVal e As EventArgs)
+		Global_asax.LogMessages.Length = 0
+		' clear the page immediately, and allow for F5 without a Postback warning.
+		Me.Response.Redirect(Me.Request.Url.AbsoluteUri)
+	End Sub
+End Class
