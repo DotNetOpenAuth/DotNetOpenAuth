9335c7d1d81f3fe67cb7f1c375dbfc305789c4b3
Andrew Arnott <andrewarnott@gmail.com>
2010-05-28 16:19:02 -0700

    Fix for outgoing check_auth messages so they don't modify positive assertion messages causing invalid signatures.
    Fixes Trac #198

--- src/DotNetOpenAuth.Test/OpenId/Messages/CheckAuthenticationRequestTests.cs
+++ src/DotNetOpenAuth.Test/OpenId/Messages/CheckAuthenticationRequestTests.cs
@@ -6,12 +6,37 @@
 
 namespace DotNetOpenAuth.Test.OpenId.Messages {
 	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
+	using DotNetOpenAuth.OpenId;
+	using DotNetOpenAuth.OpenId.Messages;
 	using NUnit.Framework;
 
 	[TestFixture]
 	public class CheckAuthenticationRequestTests : OpenIdTestBase {
+		/// <summary>
+		/// Verifies that the check_auth request is sent preserving EXACTLY (non-normalized)
+		/// what is in the positive assertion.
+		/// </summary>
+		/// <remarks>
+		/// This is very important because any normalization
+		/// (like changing https://host:443/ to https://host/) in the message will invalidate the signature
+		/// and cause the authentication to inappropriately fail.
+		/// Designed to verify fix to Trac #198.
+		/// </remarks>
+		[TestCase]
+		public void ExactPositiveAssertionPreservation() {
+			var rp = CreateRelyingParty(true);
+
+			// Initialize the positive assertion response with some data that is NOT in normalized form.
+			var positiveAssertion = new PositiveAssertionResponse(Protocol.Default.Version, RPUri)
+			{
+				ClaimedIdentifier = "https://HOST:443/a",
+				ProviderEndpoint = new Uri("https://anotherHOST:443/b"),
+			};
+
+			var checkAuth = new CheckAuthenticationRequest(positiveAssertion, rp.Channel);
+			var actual = rp.Channel.MessageDescriptions.GetAccessor(checkAuth);
+			Assert.AreEqual("https://HOST:443/a", actual["openid.claimed_id"]);
+			Assert.AreEqual("https://anotherHOST:443/b", actual["openid.op_endpoint"]);
+		}
 	}
 }
--- src/DotNetOpenAuth/Messaging/Reflection/MessagePart.cs
+++ src/DotNetOpenAuth/Messaging/Reflection/MessagePart.cs
@@ -90,14 +90,14 @@ namespace DotNetOpenAuth.Messaging.Reflection {
 				Contract.Assume(str != null);
 				return new Realm(str);
 			};
-			Map<Uri>(uri => uri.AbsoluteUri, safeUri);
-			Map<DateTime>(dt => XmlConvert.ToString(dt, XmlDateTimeSerializationMode.Utc), str => XmlConvert.ToDateTime(str, XmlDateTimeSerializationMode.Utc));
-			Map<byte[]>(safeFromByteArray, safeToByteArray);
-			Map<Realm>(realm => realm.ToString(), safeRealm);
-			Map<Identifier>(id => id.SerializedString, safeIdentifier);
-			Map<bool>(value => value.ToString().ToLowerInvariant(), safeBool);
-			Map<CultureInfo>(c => c.Name, str => new CultureInfo(str));
-			Map<CultureInfo[]>(cs => string.Join(",", cs.Select(c => c.Name).ToArray()), str => str.Split(',').Select(s => new CultureInfo(s)).ToArray());
+			Map<Uri>(uri => uri.AbsoluteUri, uri => uri.OriginalString, safeUri);
+			Map<DateTime>(dt => XmlConvert.ToString(dt, XmlDateTimeSerializationMode.Utc), null, str => XmlConvert.ToDateTime(str, XmlDateTimeSerializationMode.Utc));
+			Map<byte[]>(safeFromByteArray, null, safeToByteArray);
+			Map<Realm>(realm => realm.ToString(), realm => realm.OriginalString, safeRealm);
+			Map<Identifier>(id => id.SerializedString, id => id.OriginalString, safeIdentifier);
+			Map<bool>(value => value.ToString().ToLowerInvariant(), null, safeBool);
+			Map<CultureInfo>(c => c.Name, null, str => new CultureInfo(str));
+			Map<CultureInfo[]>(cs => string.Join(",", cs.Select(c => c.Name).ToArray()), null, str => str.Split(',').Select(s => new CultureInfo(s)).ToArray());
 		}
 
 		/// <summary>
@@ -137,15 +137,18 @@ namespace DotNetOpenAuth.Messaging.Reflection {
 						if (converters.TryGetValue(underlyingType, out underlyingMapping)) {
 							this.converter = new ValueMapping(
 								underlyingMapping.ValueToString,
+								null,
 								str => str != null ? underlyingMapping.StringToValue(str) : null);
 						} else {
 							this.converter = new ValueMapping(
 								obj => obj != null ? obj.ToString() : null,
+								null,
 								str => str != null ? Convert.ChangeType(str, underlyingType, CultureInfo.InvariantCulture) : null);
 						}
 					} else {
 						this.converter = new ValueMapping(
 							obj => obj != null ? obj.ToString() : null,
+							null,
 							str => str != null ? Convert.ChangeType(str, this.memberDeclaredType, CultureInfo.InvariantCulture) : null);
 					}
 				}
@@ -227,7 +230,7 @@ namespace DotNetOpenAuth.Messaging.Reflection {
 		}
 
 		/// <summary>
-		/// Gets the value of a member of a given message.
+		/// Gets the normalized form of a value of a member of a given message.
 		/// Used in serialization.
 		/// </summary>
 		/// <param name="message">The message instance to read the value from.</param>
@@ -235,7 +238,23 @@ namespace DotNetOpenAuth.Messaging.Reflection {
 		internal string GetValue(IMessage message) {
 			try {
 				object value = this.GetValueAsObject(message);
-				return this.ToString(value);
+				return this.ToString(value, false);
+			} catch (FormatException ex) {
+				throw ErrorUtilities.Wrap(ex, MessagingStrings.MessagePartWriteFailure, message.GetType(), this.Name);
+			}
+		}
+
+		/// <summary>
+		/// Gets the value of a member of a given message.
+		/// Used in serialization.
+		/// </summary>
+		/// <param name="message">The message instance to read the value from.</param>
+		/// <param name="originalValue">A value indicating whether the original value should be retrieved (as opposed to a normalized form of it).</param>
+		/// <returns>The string representation of the member's value.</returns>
+		internal string GetValue(IMessage message, bool originalValue) {
+			try {
+				object value = this.GetValueAsObject(message);
+				return this.ToString(value, originalValue);
 			} catch (FormatException ex) {
 				throw ErrorUtilities.Wrap(ex, MessagingStrings.MessagePartWriteFailure, message.GetType(), this.Name);
 			}
@@ -272,11 +291,20 @@ namespace DotNetOpenAuth.Messaging.Reflection {
 		/// </summary>
 		/// <typeparam name="T">The custom type to convert to and from strings.</typeparam>
 		/// <param name="toString">The function to convert the custom type to a string.</param>
+		/// <param name="toOriginalString">The mapping function that converts some custom value to its original (non-normalized) string.  May be null if the same as the <paramref name="toString"/> function.</param>
 		/// <param name="toValue">The function to convert a string to the custom type.</param>
-		private static void Map<T>(Func<T, string> toString, Func<string, T> toValue) {
+		private static void Map<T>(Func<T, string> toString, Func<T, string> toOriginalString, Func<string, T> toValue) {
+			Contract.Requires<ArgumentNullException>(toString != null, "toString");
+			Contract.Requires<ArgumentNullException>(toValue != null, "toValue");
+
+			if (toOriginalString == null) {
+				toOriginalString = toString;
+			}
+
 			Func<object, string> safeToString = obj => obj != null ? toString((T)obj) : null;
+			Func<object, string> safeToOriginalString = obj => obj != null ? toOriginalString((T)obj) : null;
 			Func<string, object> safeToT = str => str != null ? toValue(str) : default(T);
-			converters.Add(typeof(T), new ValueMapping(safeToString, safeToT));
+			converters.Add(typeof(T), new ValueMapping(safeToString, safeToOriginalString, safeToT));
 		}
 
 		/// <summary>
@@ -328,11 +356,12 @@ namespace DotNetOpenAuth.Messaging.Reflection {
 		/// Converts the member's value to its string representation.
 		/// </summary>
 		/// <param name="value">The value of the member.</param>
+		/// <param name="originalString">A value indicating whether a string matching the originally decoded string should be returned (as opposed to a normalized string).</param>
 		/// <returns>
 		/// The string representation of the member's value.
 		/// </returns>
-		private string ToString(object value) {
-			return this.converter.ValueToString(value);
+		private string ToString(object value, bool originalString) {
+			return originalString ? this.converter.ValueToOriginalString(value) : this.converter.ValueToString(value);
 		}
 
 		/// <summary>
