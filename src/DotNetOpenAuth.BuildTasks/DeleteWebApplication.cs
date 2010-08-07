8c93cbf18875b43e7e5e22529592617d8a06540c
Andrew Arnott <andrewarnott@gmail.com>
2010-08-07 14:12:55 -0700

    Backporting BuildTasks source

---
+++ src/DotNetOpenAuth.BuildTasks/DeleteWebApplication.cs
@@ -0,0 +1,66 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DeleteWebApplication.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.BuildTasks {
+	using System;
+	using System.Linq;
+	using Microsoft.Build.Framework;
+	using Microsoft.Build.Utilities;
+	using Microsoft.Web.Administration;
+
+	/// <summary>
+	/// Deletes a web application from IIS.
+	/// </summary>
+	public class DeleteWebApplication : Task {
+		/// <summary>
+		/// Gets or sets the name of the web site under which to create the web application.
+		/// </summary>
+		/// <value>The name of the existing web site.</value>
+		[Required]
+		public string WebSiteName { get; set; }
+
+		/// <summary>
+		/// Gets or sets the virtual paths within the web site that will access these applications.
+		/// </summary>
+		/// <value>The virtual path, which must start with '/'.</value>
+		[Required]
+		public ITaskItem[] VirtualPaths { get; set; }
+
+		/// <summary>
+		/// Executes this instance.
+		/// </summary>
+		/// <returns>A value indicating whether the task completed successfully.</returns>
+		public override bool Execute() {
+			var serverManager = new ServerManager();
+
+			// Find the root web site that this web application will be created under.
+			var site = serverManager.Sites.FirstOrDefault(s => string.Equals(s.Name, this.WebSiteName, StringComparison.OrdinalIgnoreCase));
+			if (site == null) {
+				Log.LogMessage(MessageImportance.Low, TaskStrings.NoMatchingWebSiteFound, this.WebSiteName);
+				return true;
+			}
+
+			if (this.VirtualPaths.Length == 0) {
+				// Nothing to do.
+				return true;
+			}
+
+			foreach (ITaskItem path in this.VirtualPaths) {
+				var app = site.Applications.FirstOrDefault(a => string.Equals(a.Path, path.ItemSpec, StringComparison.OrdinalIgnoreCase));
+				if (app != null) {
+					site.Applications.Remove(app);
+					Log.LogMessage(MessageImportance.Normal, TaskStrings.DeletedWebApplication, app.Path);
+				} else {
+					Log.LogMessage(MessageImportance.Low, TaskStrings.WebApplicationNotFoundSoNotDeleted, path.ItemSpec);
+				}
+			}
+
+			serverManager.CommitChanges();
+
+			return true;
+		}
+	}
+}
---
+++ src/DotNetOpenAuth.BuildTasks/FixupReferenceHintPaths.cs
@@ -0,0 +1,71 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FixupReferenceHintPaths.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.BuildTasks {
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Reflection;
+	using System.Text;
+	using Microsoft.Build.BuildEngine;
+	using Microsoft.Build.Framework;
+	using Microsoft.Build.Utilities;
+
+	public class FixupReferenceHintPaths : Task {
+		/// <summary>
+		/// Gets or sets the projects to fixup references for.
+		/// </summary>
+		[Required]
+		public ITaskItem[] Projects { get; set; }
+
+		/// <summary>
+		/// Gets or sets the set of full paths to assemblies that may be found in any of the <see cref="Projects"/>.
+		/// </summary>
+		[Required]
+		public ITaskItem[] References { get; set; }
+
+		/// <summary>
+		/// Executes this instance.
+		/// </summary>
+		public override bool Execute() {
+			if (this.References.Length == 0 || this.Projects.Length == 0) {
+				this.Log.LogMessage(MessageImportance.Low, "Skipping reference hintpath fixup because no projects or no references were supplied.");
+				return !this.Log.HasLoggedErrors;
+			}
+
+			// Figure out what the assembly names are of the references that are available.
+			AssemblyName[] availableReferences = new AssemblyName[this.References.Length];
+			for (int i = 0; i < this.References.Length; i++) {
+				availableReferences[i] = AssemblyName.GetAssemblyName(this.References[i].ItemSpec);
+			}
+
+			foreach (var projectTaskItem in this.Projects) {
+				var project = new Project();
+				Uri projectUri = new Uri(projectTaskItem.GetMetadata("FullPath"));
+				project.Load(projectTaskItem.ItemSpec);
+
+				foreach (BuildItem referenceItem in project.GetEvaluatedItemsByName("Reference")) {
+					var referenceAssemblyName = new AssemblyName(referenceItem.Include);
+					var matchingReference = availableReferences.FirstOrDefault(r => string.Equals(r.Name, referenceAssemblyName.Name, StringComparison.OrdinalIgnoreCase));
+					if (matchingReference != null) {
+						var originalSuppliedReferenceItem = this.References[Array.IndexOf(availableReferences, matchingReference)];
+						string hintPath = originalSuppliedReferenceItem.GetMetadata("HintPath");
+						if (string.IsNullOrEmpty(hintPath)) {
+							hintPath = projectUri.MakeRelativeUri(new Uri(matchingReference.CodeBase)).OriginalString.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+						}
+						this.Log.LogMessage("Fixing up HintPath to \"{0}\" in project \"{1}\".", referenceAssemblyName.Name, projectTaskItem.ItemSpec);
+						referenceItem.SetMetadata("HintPath", hintPath);
+					}
+				}
+
+				project.Save(projectTaskItem.ItemSpec);
+			}
+
+			return !this.Log.HasLoggedErrors;
+		}
+	}
+}
