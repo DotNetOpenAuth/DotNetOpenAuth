4a25e81697e3e68bb617fd6feb07c2beed623bfc
Andrew Arnott <andrewarnott@gmail.com>
2011-07-31 19:43:46 -0600

    Split up InfoCard into two assemblies.

--- src/DotNetOpenAuth.OpenId/Properties/AssemblyInfo.cs
+++ src/DotNetOpenAuth.OpenId/Properties/AssemblyInfo.cs
@@ -17,8 +17,6 @@ using System.Security;
 using System.Security.Permissions;
 using System.Web.UI;
 
-[assembly: TagPrefix("DotNetOpenAuth.OpenId", "openid")]
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
