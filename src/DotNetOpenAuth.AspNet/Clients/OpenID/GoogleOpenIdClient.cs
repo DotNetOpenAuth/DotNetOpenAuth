00513ca60af80fd0ef97db4ee9cce21ab7e8a859
dotnetjunky <dotnetjunky@live.com>
2011-12-20 17:52:24 -0800

    Make changes per discussion. Remove the DNOA.WebPages project. Rename DNOA.Web to DNOA.AspNet.

---
+++ src/DotNetOpenAuth.AspNet/Clients/OpenID/GoogleOpenIdClient.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
+using DotNetOpenAuth.OpenId.RelyingParty;
+
+namespace DotNetOpenAuth.AspNet.Clients
+{
+    /// <summary>
+    /// Represents Google OpenID client.
+    /// </summary>
+    public sealed class GoogleOpenIdClient : OpenIDClient
+    {
+        public GoogleOpenIdClient() :
+            base("google", "https://www.google.com/accounts/o8/id")
+        {
+        }
+
+        /// <summary>
+        /// Called just before the authentication request is sent to service provider.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        protected override void OnBeforeSendingAuthenticationRequest(IAuthenticationRequest request)
+        {
+            // Attribute Exchange extensions
+            var fetchRequest = new FetchRequest();
+            fetchRequest.Attributes.Add(new AttributeRequest(WellKnownAttributes.Contact.Email, isRequired: true));
+            fetchRequest.Attributes.Add(new AttributeRequest(WellKnownAttributes.Contact.HomeAddress.Country, isRequired: false));
+            fetchRequest.Attributes.Add(new AttributeRequest(WellKnownAttributes.Name.First, isRequired: false));
+            fetchRequest.Attributes.Add(new AttributeRequest(WellKnownAttributes.Name.Last, isRequired: false));
+
+            request.AddExtension(fetchRequest);
+        }
+
+        /// <summary>
+        /// Gets the extra data obtained from the response message when authentication is successful.
+        /// </summary>
+        /// <param name="response">The response message.</param>
+        /// <returns></returns>
+        protected override Dictionary<string, string> GetExtraData(IAuthenticationResponse response)
+        {
+            FetchResponse fetchResponse = response.GetExtension<FetchResponse>();
+            if (fetchResponse != null)
+            {
+                var extraData = new Dictionary<string, string>();
+                extraData.AddItemIfNotEmpty("email", fetchResponse.GetAttributeValue(WellKnownAttributes.Contact.Email));
+                extraData.AddItemIfNotEmpty("country", fetchResponse.GetAttributeValue(WellKnownAttributes.Contact.HomeAddress.Country));
+                extraData.AddItemIfNotEmpty("firstName", fetchResponse.GetAttributeValue(WellKnownAttributes.Name.First));
+                extraData.AddItemIfNotEmpty("lastName", fetchResponse.GetAttributeValue(WellKnownAttributes.Name.Last));
+
+                return extraData;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/DotNetOpenAuth.AspNet/IAuthenticationClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+
+namespace DotNetOpenAuth.AspNet
+{
+    /// <summary>
+    /// Represents a client which can authenticate users via an external website/provider.
+    /// </summary>
+    public interface IAuthenticationClient
+    {
+        /// <summary>
+        /// Gets the name of the provider which provides authentication service.
+        /// </summary>
+        string ProviderName { get; }
+
+        /// <summary>
+        /// Attempts to authenticate users by forwarding them to an external website, and
+        /// upon succcess or failure, redirect users back to the specified url.
+        /// </summary>
+        /// <param name="context">The context of the current request.</param>
+        /// <param name="returnUrl">The return url after users have completed authenticating against external website.</param>
+        void RequestAuthentication(HttpContextBase context, Uri returnUrl);
+
+        /// <summary>
+        /// Check if authentication succeeded after user is redirected back from the service provider.
+        /// </summary>
+        /// <param name="context">The context of the current request.</param>
+        /// <returns>
+        /// An instance of <see cref="AuthenticationResult"/> containing authentication result.
+        /// </returns>
+        AuthenticationResult VerifyAuthentication(HttpContextBase context);
+    }
+}
