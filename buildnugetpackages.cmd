4868fa5e6ac105c66d1470e1b76d2b22b74f6a4a
Andrew Arnott <andrewarnott@gmail.com>
2012-02-28 07:44:33 -0800

    Deleted extra script file.

--- buildnugetpackages.cmd
+++
@@ -1,4 +0,0 @@
-.\tools\nuget\nuget.exe pack .\nugetpackages\metadata\DNOA.Core.1.0.nuspec -outputdirectory .\nugetpackages
-.\tools\nuget\nuget.exe pack .\nugetpackages\metadata\DNOA.OAuth.Consumer.1.0.nuspec -outputdirectory .\nugetpackages
-.\tools\nuget\nuget.exe pack .\nugetpackages\metadata\DNOA.OpenId.RelyingParty.1.0.nuspec -outputdirectory .\nugetpackages
-.\tools\nuget\nuget.exe pack .\nugetpackages\metadata\DNOA.AspNet.1.0.nuspec -outputdirectory .\nugetpackages
