ed71e7109016111e8b72bf759fdf5df8a6931dd0
Andrew Arnott <andrewarnott@gmail.com>
2012-04-05 20:24:05 -0700

    Reverts v4.0 branch back to VS2010, and samples target .NET 3.5, and MVC samples are MVC 2.
    The "upgrade" had introduced a lot of issues, such as broken project templates, broken MVC 3/4 story, etc. and wasn't fitting the stabilization that the v4.0 branch needs.

---
+++ projecttemplates/RelyingPartyDatabase/Schema Objects/Database Level Objects/Storage/Files/Database.mdf.sqlfile.sql
@@ -0,0 +1,3 @@
+﻿ALTER DATABASE [$(DatabaseName)]
+    ADD FILE (NAME = [$(Path1)$(DatabaseName).mdf], FILENAME = '$(Path1)$(DatabaseName).mdf', MAXSIZE = UNLIMITED, FILEGROWTH = 1024 KB) TO FILEGROUP [PRIMARY];
+
