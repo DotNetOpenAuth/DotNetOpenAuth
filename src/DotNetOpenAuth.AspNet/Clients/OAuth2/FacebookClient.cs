7720ea2a340ab1250da1bdbb39bf8461d8b3db1a
Andrew Arnott <andrewarnott@gmail.com>
2012-10-02 20:39:50 -0700

    Style fix.

--- src/DotNetOpenAuth.AspNet/Clients/OAuth2/FacebookClient.cs
+++ src/DotNetOpenAuth.AspNet/Clients/OAuth2/FacebookClient.cs
@@ -79,7 +79,7 @@ namespace DotNetOpenAuth.AspNet.Clients {
 				new Dictionary<string, string> {
 					{ "client_id", this.appId },
 					{ "redirect_uri", returnUrl.AbsoluteUri },
-					{ "scope", "email" }
+					{ "scope", "email" },
 					
 				});
 			return builder.Uri;
@@ -135,7 +135,7 @@ namespace DotNetOpenAuth.AspNet.Clients {
 					{ "redirect_uri", NormalizeHexEncoding(returnUrl.AbsoluteUri) },
 					{ "client_secret", this.appSecret },
 					{ "code", authorizationCode },
-					{ "scope", "email" }
+					{ "scope", "email" },
 				});
 
 			using (WebClient client = new WebClient()) {
