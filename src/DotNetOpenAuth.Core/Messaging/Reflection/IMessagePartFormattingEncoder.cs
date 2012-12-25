d3f4149dd35b85b7ce00cb9a4b3208cab6065b86
Andrew Arnott <andrewarnott@gmail.com>
2012-12-24 16:25:40 -0800

    Fixed stylecop errors.

--- src/DotNetOpenAuth.Core/Messaging/Reflection/IMessagePartFormattingEncoder.cs
+++ src/DotNetOpenAuth.Core/Messaging/Reflection/IMessagePartFormattingEncoder.cs
@@ -1,4 +1,10 @@
-﻿namespace DotNetOpenAuth.Messaging.Reflection {
+﻿//-----------------------------------------------------------------------
+// <copyright file="IMessagePartFormattingEncoder.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace DotNetOpenAuth.Messaging.Reflection {
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
