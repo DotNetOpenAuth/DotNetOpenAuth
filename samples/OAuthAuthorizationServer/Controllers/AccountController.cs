6877b41cb57087bc6ecefcfc5a0f9534a33b2ba8
Andrew Arnott <andrewarnott@gmail.com>
2013-03-27 21:33:17 -0700

    Removes some optional cancellation tokens that don't work with all server configurations.

--- samples/OAuthAuthorizationServer/Controllers/AccountController.cs
+++ samples/OAuthAuthorizationServer/Controllers/AccountController.cs
@@ -23,13 +23,13 @@
 		public async Task<ActionResult> LogOn(LogOnModel model, string returnUrl) {
 			if (ModelState.IsValid) {
 				var rp = new OpenIdRelyingParty();
-				var request = await rp.CreateRequestAsync(model.UserSuppliedIdentifier, Realm.AutoDetect, new Uri(Request.Url, Url.Action("Authenticate")), Response.ClientDisconnectedToken);
+				var request = await rp.CreateRequestAsync(model.UserSuppliedIdentifier, Realm.AutoDetect, new Uri(Request.Url, Url.Action("Authenticate")));
 				if (request != null) {
 					if (returnUrl != null) {
 						request.AddCallbackArguments("returnUrl", returnUrl);
 					}
 
-					var response = await request.GetRedirectingResponseAsync(Response.ClientDisconnectedToken);
+					var response = await request.GetRedirectingResponseAsync();
 					return response.AsActionResult();
 				} else {
 					ModelState.AddModelError(string.Empty, "The identifier you supplied is not recognized as a valid OpenID Identifier.");
@@ -42,7 +42,7 @@
 
 		public async Task<ActionResult> Authenticate(string returnUrl) {
 			var rp = new OpenIdRelyingParty();
-			var response = await rp.GetResponseAsync(Request, Response.ClientDisconnectedToken);
+			var response = await rp.GetResponseAsync(Request);
 			if (response != null) {
 				switch (response.Status) {
 					case AuthenticationStatus.Authenticated:
