789f14adf18e65ab416b60341bfbecc6577a1c37
Girish Bablani <girishb@microsoft.com>
2013-04-16 18:53:13 -0700

    Enabled AzureAD integration and added TestAzureAD sample app

---
+++ samples/TestAzureAD/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using TestAzureAD;
+
+namespace TestAzureAD
+{
+	public class Global : HttpApplication
+	{
+		void Application_Start(object sender, EventArgs e)
+		{
+			// Code that runs on application startup
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			AuthConfig.RegisterOpenAuth();
+		}
+
+		void Application_End(object sender, EventArgs e)
+		{
+			//  Code that runs on application shutdown
+
+		}
+
+		void Application_Error(object sender, EventArgs e)
+		{
+			// Code that runs when an unhandled error occurs
+
+		}
+	}
+}
