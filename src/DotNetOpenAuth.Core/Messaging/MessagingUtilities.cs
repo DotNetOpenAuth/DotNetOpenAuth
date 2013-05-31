d8a034db629baa0e2a86528eac5d0ab49d9bb984
Andrew Arnott <andrewarnott@gmail.com>
2013-05-31 08:55:57 -0700

    Replaces hard-coded algorithms for parameterized ones.
    Closes #285

--- src/DotNetOpenAuth.Core/Messaging/MessagingUtilities.cs
+++ src/DotNetOpenAuth.Core/Messaging/MessagingUtilities.cs
@@ -873,7 +873,7 @@ namespace DotNetOpenAuth.Messaging {
 			Requires.NotNull(crypto, "crypto");
 			Requires.NotNull(buffer, "buffer");
 
-			using (var symmetricCrypto = new RijndaelManaged()) {
+			using (var symmetricCrypto = SymmetricAlgorithm.Create()) {
 				symmetricCrypto.Mode = CipherMode.CBC;
 
 				using (var encryptedStream = new MemoryStream()) {
@@ -915,7 +915,7 @@ namespace DotNetOpenAuth.Messaging {
 				byte[] encryptedPrequel = encryptedStreamReader.ReadBuffer(4096);
 				byte[] prequel = crypto.Decrypt(encryptedPrequel, false);
 
-				using (var symmetricCrypto = new RijndaelManaged()) {
+				using (var symmetricCrypto = SymmetricAlgorithm.Create()) {
 					symmetricCrypto.Mode = CipherMode.CBC;
 
 					byte[] symmetricKey = new byte[symmetricCrypto.Key.Length];
@@ -2034,7 +2034,7 @@ namespace DotNetOpenAuth.Messaging {
 		private static SymmetricAlgorithm CreateSymmetricAlgorithm(byte[] key) {
 			SymmetricAlgorithm result = null;
 			try {
-				result = new RijndaelManaged();
+				result = SymmetricAlgorithm.Create();
 				result.Mode = CipherMode.CBC;
 				result.Key = key;
 				return result;
