ef21fed4150b6dba853f518a8467e268bb209ab7
Andrew Arnott <andrewarnott@gmail.com>
2013-06-09 17:24:41 -0700

    Fixes ThreadAbortException that was preventing the verifier being saved with the request token.

--- samples/OAuthServiceProvider/Members/Authorize.aspx.cs
+++ samples/OAuthServiceProvider/Members/Authorize.aspx.cs
@@ -63,7 +63,6 @@
 						if (response != null) {
 							var responseMessage = await sp.Channel.PrepareResponseAsync(response, Response.ClientDisconnectedToken);
 							await responseMessage.SendAsync();
-							this.Context.Response.End();
 						} else {
 							if (pending.IsUnsafeRequest) {
 								this.verifierMultiView.ActiveViewIndex = 1;
