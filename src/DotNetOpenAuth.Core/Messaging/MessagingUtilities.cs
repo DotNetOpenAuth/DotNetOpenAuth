9fac617a455acffa4f78dc5cfe7415157e12a63c
Andrew Arnott <andrewarnott@gmail.com>
2013-10-19 18:08:25 -0700

    Moves all MVC dependencies from core assemblies to a separate assembly.
    I still need to add DNOA.Mvc to some build and package authoring.

--- src/DotNetOpenAuth.Core/Messaging/MessagingUtilities.cs
+++ src/DotNetOpenAuth.Core/Messaging/MessagingUtilities.cs
@@ -24,7 +24,6 @@ namespace DotNetOpenAuth.Messaging {
 	using System.Threading;
 	using System.Threading.Tasks;
 	using System.Web;
-	using System.Web.Mvc;
 	using System.Xml;
 	using DotNetOpenAuth.Messaging.Bindings;
 	using DotNetOpenAuth.Messaging.Reflection;
@@ -408,16 +407,6 @@ namespace DotNetOpenAuth.Messaging {
 		public static Uri GetPublicFacingUrl() {
 			ErrorUtilities.VerifyHttpContext();
 			return GetPublicFacingUrl(new HttpRequestWrapper(HttpContext.Current.Request));
-		}
-
-		/// <summary>
-		/// Wraps a response message as an MVC <see cref="ActionResult"/> so it can be conveniently returned from an MVC controller's action method.
-		/// </summary>
-		/// <param name="response">The response message.</param>
-		/// <returns>An <see cref="ActionResult"/> instance.</returns>
-		public static ActionResult AsActionResult(this HttpResponseMessage response) {
-			Requires.NotNull(response, "response");
-			return new HttpResponseMessageActionResult(response);
 		}
 
 		/// <summary>
@@ -2042,34 +2031,6 @@ namespace DotNetOpenAuth.Messaging {
 			}
 
 			#endregion
-		}
-
-		/// <summary>
-		/// An MVC <see cref="ActionResult"/> that wraps an <see cref="HttpResponseMessage"/>
-		/// </summary>
-		private class HttpResponseMessageActionResult : ActionResult {
-			/// <summary>
-			/// The wrapped response.
-			/// </summary>
-			private readonly HttpResponseMessage response;
-
-			/// <summary>
-			/// Initializes a new instance of the <see cref="HttpResponseMessageActionResult"/> class.
-			/// </summary>
-			/// <param name="response">The response.</param>
-			internal HttpResponseMessageActionResult(HttpResponseMessage response) {
-				Requires.NotNull(response, "response");
-				this.response = response;
-			}
-
-			/// <summary>
-			/// Enables processing of the result of an action method by a custom type that inherits from the <see cref="T:System.Web.Mvc.ActionResult" /> class.
-			/// </summary>
-			/// <param name="context">The context in which the result is executed. The context information includes the controller, HTTP content, request context, and route data.</param>
-			public override void ExecuteResult(ControllerContext context) {
-				// Sadly, MVC doesn't support writing to the response stream asynchronously.
-				this.response.SendAsync(context.HttpContext).GetAwaiter().GetResult();
-			}
 		}
 	}
 }
---
+++ src/DotNetOpenAuth.Mvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCopyright("Copyright © 2012 Outercurve Foundation")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bbb01d47-0265-4423-82a2-77be311ca7c6")]
